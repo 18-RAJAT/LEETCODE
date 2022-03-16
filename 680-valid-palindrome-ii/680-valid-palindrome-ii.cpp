cb3bec1cc47e2382951b7b2c7f1f3759290b04b7
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-03-16 06:57:34 +0530

    Time: 80 ms (35.88%), Space: 24 MB (30.67%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    bool CheckPalindrome(string s , int start , int end)
+    {
+        while(start <= end)
+        {
+            if(s[start] != s[end])
+            {
+               return false;
+            }
+            
+            start++;
+            end--;
+        }
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+     
+        int left = 0;
+        int right = s.size() - 1;
+        
+        while(left <= right)
+        {
+            if(s[left] != s[right])
+            {
+                if(CheckPalindrome(s,left+1,right) or CheckPalindrome(s,left,right-1))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+        
+    }
+};
