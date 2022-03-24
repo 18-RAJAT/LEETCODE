1e5b49bbf2982ed1a2926a7efb61986778a3fb3f
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-03-24 07:15:54 +0530

    Time: 87 ms (78.28%), Space: 41.8 MB (91.53%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        
+        //Sort all of people 
+        sort(people.begin(),people.end());
+        
+        int i = 0;
+        int j = people.size() -1;
+        
+        //Initially count value is 0
+        int count = 0;
+        
+        while(i <= j)
+        {
+            //This will represent that add light weight people with heavy weight people
+            if(people[i] + people[j] <= limit)
+                
+                i++;
+                j--;
+                count++;
+        }
+        
+        return count;
+        
+    }
+};
