f5504eabd0a39ba32014c5ba6819fd4834a9fee1
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-03-29 08:40:24 +0530

    Time: 4 ms (53.09%), Space: 7.3 MB (48.42%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,77 +1,32 @@
-
-
-
-
-class Solution{
+class Solution {
 public:
     
     int n;
     vector<vector<int>>answer;
     
-    vector<int> Pair;
+    vector<int>pair;
     
-    vector<vector<int>> subsets(vector<int>& nums){
-        
+    vector<vector<int>> subsets(vector<int>& nums) {
+    
         n = nums.size();
         backtrack(0,nums);
         return answer;
-        
     }
     
-    void backtrack(int idx,vector<int>& nums)
+    void backtrack(int index,vector<int>& nums)
     {
-        if(idx == n)
+        if(index == n)
         {
-            answer.push_back(Pair);
+            answer.push_back(pair);
         }
         
         else
         {
-            Pair.push_back(nums[idx]);
-            backtrack(idx+1,nums);
-            Pair.pop_back();
-            backtrack(idx+1,nums);
+            pair.push_back(nums[index]);
+            backtrack(index+1,nums);
+            pair.pop_back();
+            backtrack(index+1,nums);
         }
     }
     
 };
-
-
-
-// class Solution {
-// public:
-    
-//     vector<vector<int>>ans;
-    
-//     void helper (int i,vector<int>&nums,vector<int>&temp)
-//     {
-//         if(i == nums.size())
-//         {
-//             ans.push_back(temp);
-//         }
-        
-//         else
-//         {
-//             //To Get The ith Element
-//             temp.push_back(nums[i]);
-            
-            
-//            helper (i+1,nums,temp);
-//             temp.pop_back();
-            
-            
-//             //To ignore The ith element
-//             helper (i+1,nums,temp);
-//         }
-//     }
-//     vector<vector<int>> subsets(vector<int>& nums) {
-        
-//             vector<int>temp;
-            
-//             helper (0,nums,temp);
-            
-//             return ans;
-        
-//     }
-// };
-
