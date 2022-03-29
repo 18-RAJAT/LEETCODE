e0b51adb4fc3c93e0b5c6562b765ec274ab60cb1
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-03-29 13:33:40 +0530

    Time: 7 ms (84.43%), Space: 11.2 MB (60.40%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -12,29 +12,36 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         
-        ListNode *dummy=new ListNode(0,head);
-       ListNode *prev=dummy;
+        //Creating a dummy node
+        ListNode*dummy = new ListNode(0,head);
         
-        while(head!=NULL)
+        ListNode* previous = dummy;
+        
+        //Base case
+        while(head != NULL)
         {
-       
-        if(head->next!=NULL and head->val==head->next->val)
-        {
+            if(head->next != NULL and head->val == head->next->val)
+            {
+                //Again intering the base case 
+                while(head->next != NULL and head->val == head->next->val)
                 
-       while(head->next!=NULL and head->val==head->next->val)
-           head=head->next;
-                     
-                prev->next=head->next;
-        }
+                    head = head->next;
                 
-     else 
-                    
-            prev=prev->next;
+                //Here we will point at first previous ka next then head ka next   
+                previous->next = head->next; 
+            }   
             
-            head=head->next;
+            else
+                
+                //initilize the previous = previous point to the next node 
+                previous = previous->next;
+                
+                //And then we will be doing this ki head will be point the next node 
+                head = head->next;
             
         }
         
+        //At the end we will be return the dummy will we pointing to the next then this thing we will be return
         return dummy->next;
         
     }
