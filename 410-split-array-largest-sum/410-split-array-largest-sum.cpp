aaf9c3a1da254c2373a36c600a1b74b13ab899d8
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-03-31 06:31:35 +0530

    Time: 7 ms (35.73%), Space: 7.2 MB (74.76%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,87 +1,88 @@
 class Solution {
 public:
+
+bool isValid(vector<int>&nums,int m, int maximum)
+{
+    //Starting with 1st index
+    int index = 1;
+    //Initially sum is 0
+    int sum = 0; 
     
-    bool isValid(vector<int>&nums,int m, int maximum)
+    //Traverse the array
+    for(int i=0;i<nums.size();++i)
     {
-        //Starting with 1st index
-        int index = 1;
-        //Initially sum is 0
-        int sum = 0; 
+        //Initially sum = 0 then we will add nums value repeatedly when the range is not end
+        sum += nums[i];
         
-        //Traverse the array
-        for(int i=0;i<nums.size();++i)
+        //Base case
+        if(sum > maximum)
         {
-            //Initially sum = 0 then we will add nums value repeatedly when the range is not end
-            sum += nums[i];
+            //Index value increase to the next index 
+            index++;
             
-            //Base case
-            if(sum > maximum)
-            {
-                //Index value increase to the next index 
-                index++;
-                
-                //Sum stores the  nums value idx
-                sum = nums[i];
-            }
-            
-            //Edge case(index value is split array to greater then false the statement)
-            if(index > m)
-            {
-               return false;
-            }
+            //Sum stores the  nums value idx
+            sum = nums[i];
         }
         
-        return true;
+        //Edge case(index value is split array to greater then false the statement)
+        if(index > m)
+        {
+           return false;
+        }
     }
     
-    int splitArray(vector<int>& nums, int m) {
-        
-        int max = -1;//Maximum at first initilize -1
-        int sum =  0;//Sum value is 0
-        
-        for(int i : nums)
+    return true;
+}
+//We can use Binary search to find the value x0. Keeping a valuemid = (left + right) / 2. IF(mid)is false, then we will search the range[mid + 1, right];
+//IF(mid)is true, then we will search[left, mid - 1]
+
+int splitArray(vector<int>& nums, int m) {
+    int max = -1;//Maximum at first initilize -1
+    int sum =  0;//Sum value is 0
+    
+    for(int i : nums)
+    {
+        //nums value(i value) is i value is greater then maximum then store into i value in maximum 
+        if(i > max)
         {
-            //nums value(i value) is i value is greater then maximum then store into i value in maximum 
-            if(i > max)
-            {
-                max = i;
-            }
-            
-            //else we will be adding i value in sum
-            sum += i;
+            max = i;
         }
         
-        /*
-        Initialization:
-        start = max
-        end = sum
-        answer = -1 (index to index very...!!)
-        */
-        int start = max;
-        int end = sum;
-        int answer = 0;
-        
-        //Using binary search
-        while(start <= end)
-        {
-            //Finding middle element
-            int mid = start + (end - start)/2;
-            
-            if(isValid(nums,m,mid))
-            {
-                //Previous condition is stisfied then
-                answer = mid;
-                //End will store middle element - 1
-                end = mid -1;
-            }
-            
-            else
-            {
-                //previous condition not stisfied then we will be store middle element + 1
-                start = mid + 1;
-            }
-        }
-        //At last we will store the maximum answer then return the answer
-        return answer;
+        //else we will be adding i value in sum
+        sum += i;
     }
+    
+    /*
+    Initialization:
+    start = max
+    end = sum
+    answer = -1 (index to index very...!!)
+    */
+    int start = max;
+    int end = sum;
+    int answer = 0;
+    
+    //Using binary search
+    while(start <= end)
+    {
+        //Finding middle element
+        int mid = start + (end - start)/2;
+        
+        if(isValid(nums,m,mid))
+        {
+            //Previous condition is stisfied then
+            answer = mid;
+            //End will store middle element - 1
+            end = mid -1;
+        }
+        
+        else
+        {
+            //previous condition not stisfied then we will be store middle element + 1
+            start = mid + 1;
+        }
+    }
+    //At last we will store the maximum answer then return the answer
+    return answer;
+}
 };
