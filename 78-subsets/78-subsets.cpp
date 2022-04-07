170dafdc9d8ccf74433da268a8a74571ffa828b5
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-04-07 21:02:58 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (48.31%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     
-    int n;
+   int n;
+    
+    //Store the answer in answer vector
     vector<vector<int>>answer;
     
+    //Pair to Pair we will take the answer variables
     vector<int>pair;
     
+    //Subset Function we will write to use or taking the nums and backtrack call and then return answer....!!
     vector<vector<int>> subsets(vector<int>& nums) {
     
         n = nums.size();
         backtrack(0,nums);
         return answer;
     }
+    
+    //This function is works on like when the pair we will created then again backtrack and find the pair of the function....!!
     
     void backtrack(int index,vector<int>& nums)
     {
@@ -22,11 +28,18 @@ public:
         
         else
         {
+            
+            //This will given pair
             pair.push_back(nums[index]);
+            
+            //Backtracking
             backtrack(index+1,nums);
+            
+            //When the pair will be created then  the pair will be poped out
             pair.pop_back();
+            
+            //When one pair is created then next pair will be create again again again when the string is not over...!!
             backtrack(index+1,nums);
         }
     }
-    
 };
