992afeecf3bdce17f8100c789da98ef7a04caa14
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-04-08 19:02:55 +0530

    Time: 11 ms (56.19%), Space: 5.9 MB (92.37%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+       
+        const int value[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        const string roman[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+      string intToRoman(int num) {
+          
+          string answer = "";
+          
+          for(int i=0;num;++i)
+          {
+              while(num >= value[i])
+              {
+                  answer += roman[i],num = num - value[i];
+              }
+          }              
+          
+          return answer;
+    }
+};
