d47cb9076ead932a660c464c61e426639d3bd020
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-04-12 22:55:53 +0530

    Time: 93 ms (75.88%), Space: 21.3 MB (39.72%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-           
-        vector<vector<int>>answer;
+        
+         vector<vector<int>>answer;
         
         sort(nums.begin(),nums.end());
         
         for(int i=0;i<nums.size();++i)
         {
             int target = -nums[i];
-            int front = i+1;
-            int back = nums.size() - 1;
             
-            while(front < back)
+            //Starting position
+            int left = i+1;
+            //Ending position
+            int right = nums.size() - 1;
+            
+            while(left < right)
             {
-                int sum = nums[front] + nums[back];
+                int sum = nums[left] + nums[right];
                 
                 // Finding answer which start from number num[i]
                 if(sum < target)
-                    front++;
+                    left++;
                 
                 else if(sum > target)
-                    back--;
+                    right--;
                 
                 else
                 {
-                    vector<int>triplet = {nums[i],nums[front],nums[back]};
+                    vector<int>triplet = {nums[i],nums[left],nums[right]};
                     
                     answer.push_back(triplet);
                     
-                    //Rolling the front pointer next different number forward
-                    while(front < back and nums[front] == triplet[1])
-                        front++;
+                    while(left < right and nums[left] == triplet[1])
+                        left++;
                     
-                    //Roalling the back pointer next different number backword
-                    while(front < back and nums[front] == triplet[2])
-                        back--;
+                    while(left < right and nums[left] == triplet[2])
+                        right--;
                 }
             }
             while(i+1 < nums.size() and nums[i+1] == nums[i])
