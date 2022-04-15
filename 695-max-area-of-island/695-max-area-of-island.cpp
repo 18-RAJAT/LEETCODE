55d7e4d5f555191c219e81d6a1f235123382eda6
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-04-15 20:36:54 +0530

    Time: 17 ms (84.33%), Space: 23.2 MB (88.97%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,7 +1,9 @@
 class Solution {
     
+    //Dfs call 
     int dfs(vector<vector<int>>& grid,int i,int j)
     {
+         
         int row = grid.size();
         int column = grid[0].size();
         
@@ -12,22 +14,30 @@ class Solution {
         
         grid[i][j] = 2;
         
+        //up down left right side call
         return(1 + dfs(grid,i+1,j) + dfs(grid,i-1,j) + dfs(grid,i,j+1) + dfs(grid,i,j-1));
     }
     
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         
+        //Row side
         int row = grid.size();
+        
+        //Column side
         int column = grid[0].size();
         
         int result = 0;
+        
+        //Taking input row and column to traverse both side
         for(int i=0;i<row;++i)
         {
             for(int j=0;j<column;++j)
             {
+                //row column traverse then we will be finding where the 1 is present in the island
                 if(grid[i][j] == 1)
                 {
+                    //Then we will call dfs 
                     result = max(result,dfs(grid,i,j));
                 }
             }
