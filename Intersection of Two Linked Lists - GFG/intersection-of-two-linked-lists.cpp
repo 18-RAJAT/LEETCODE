d0d23f39f346ce5360f4ee35406219943d510d98
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-04-16 10:23:13 +0530

    Added solution - LeetHub

---
+++ Intersection of Two Linked Lists - GFG/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,123 @@
+// { Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+
+ // } Driver Code Ends
+/* structure of list node:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution{
+  public:
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+        // code here
+        // return the head of intersection list
+        
+      Node* p1=head1;
+      Node* p2=head2;
+      
+      Node* curr=new Node(0);
+      
+      Node* head=curr;
+      
+      unordered_set<int>s;
+      
+      while(p2!=NULL)
+      {
+          s.insert(p2->data);
+          p2=p2->next;
+      }
+      
+      while(p1!=NULL)
+      {
+          if(s.find(p1->data)!=s.end())
+          {
+              Node* t=new Node(p1->data);
+              curr->next=t;
+              curr=curr->next;
+          }
+          p1=p1->next;
+      }
+      head=head->next;
+      return head;
+  
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    
+	    cin>> n;
+	    Node* head1 = inputList(n);
+	    
+	    cin>>m;
+	    Node* head2 = inputList(m);
+	    Solution obj;
+	    Node* result = obj.findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
