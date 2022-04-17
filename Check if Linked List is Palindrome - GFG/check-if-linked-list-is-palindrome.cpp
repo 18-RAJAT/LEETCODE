1d8380b532e62b98320695c8425731979e3eee8c
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-04-17 09:21:16 +0530

    Added solution - LeetHub

---
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -0,0 +1,97 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+
+ // } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution{
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+        //Your code here
+        Node* current = head;
+        string s = "",reverse = "";
+        
+        while(current)
+        {
+            s +=current->data;
+            current = current->next;
+            
+        }
+        
+        for(int i = s.length() - 1;i>=0;--i)
+        {
+            reverse = reverse + s[i];
+        }
+        
+        for(int i=0;i<s.length();++i)
+        {
+            if(s[i] == reverse[i])
+            {
+                continue;
+            }
+            
+            else
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
+// { Driver Code Starts.
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+   	cout<<obj.isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
