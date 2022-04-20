fb7254af17761ee9ab403ce41e0f9bed092705fc
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-04-21 00:13:49 +0530

    Time: 26 ms (67.21%), Space: 16.3 MB (91.73%) - LeetHub

---
+++ 155-min-stack/155-min-stack.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+    private:
+    stack<int>s1;
+    stack<int>s2;
+    
+    
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s1.push(val);
+        
+        if(s2.empty() or val <= getMin()) 
+        
+            s2.push(val);
+        
+    }
+    
+    void pop() {
+        if(s1.top() == getMin())
+        
+        s2.pop();
+        s1.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+};
+
