f2bd981cd5bc77e5b5e2ac434ef14441423d0948
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-04-21 17:38:59 +0530

    Time: 191 ms (33.72%), Space: 43.4 MB (46.70%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.cpp
@@ -0,0 +1,21 @@
+class MyHashSet {
+
+public:
+vector<bool> table;
+    
+public:
+    
+MyHashSet() : table(1e6 + 1, false) {}
+	
+	void add(int key) {
+		table[key] = true;
+	}
+	
+	void remove(int key) {
+		table[key] = false;
+	}
+	
+	bool contains(int key) {
+		return table[key];
+    }
+};
