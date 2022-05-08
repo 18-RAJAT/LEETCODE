30e7b583c8b846cba315a8905466b6b722fed555
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-09 00:23:33 +0530

    Time: 175 ms (42.19%), Space: 61.3 MB (61.94%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,71 +1,33 @@
-class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
-        
-        
-//BRUTE FORCE APPROACH.....!!
-        int index = 0;
-        
-        //Sort
-        sort(nums.begin(),nums.end());
-        
-        for(int i=0;i<nums.size()-1;++i)
-     {
-                //Break this loop
-            if(nums[i] == nums[i+1])
-            {
-                index = nums[i];
-                break;
-            }
+class Solution{
+    public:
+    int findDuplicate(vector<int>&nums)
+    {
+       int start = 0;
+       int end = nums.size()-1;
+        int ct;
             
-      }
-        
-        return index;
-        
+            while(start <= end)
+            {
+                    int mid = start + (end - start)/2;
+                    ct = 0;
+                    
+                    for(auto n : nums)
+                    {
+                            if(n <= mid)
+                            {
+                                    ct++;
+                            }
+                    }
+                    
+                    if(ct <= mid)
+                    {
+                            start = mid + 1;
+                    }
+                    else
+                    {
+                            end = mid - 1;
+                    }
+            }
+            return start;
     }
 };
-
-
-
-
-
-// //USING BINARY SEARCH...!!
-
-// class Solution {
-// public:
-    
-//     int findDuplicate(vector<int>& nums) {
-  
-//         int start = 1;
-//         int end = nums.size()-1;
-//         int count;
-        
-//         while(start <= end)
-//         {
-//             int mid = start + (end - start)/2;
-//             count = 0;
-            
-//             for(int n  : nums)
-//             {
-//                 if(n <= mid)
-//                 {
-//                     count++;
-//                 }
-//             }
-            
-//             //Binary search left side
-//             if(count <= mid)
-//             {
-//                 start = mid + 1;
-//             }
-            
-//             //Binary search right side
-//             else
-//             {
-//                 end = mid - 1;
-//             }
-//         }
-//         return start;
-//     }
-// };
-
