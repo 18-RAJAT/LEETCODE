a08b750466ae5cdf050f0d4958a6273d9d305e3d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-09 00:28:34 +0530

    Time: 9 ms (8.85%), Space: 8.4 MB (25.46%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        
-        int first = 0;
-        int mid = 0;
-        int last = nums.size()-1;
-        
-        while(mid <= last)
-        {
-            //Using Switch cases...!!
-            switch(nums[mid])
-            {
-                case 0: swap(nums[first++] , nums[mid++]);
-                    break;
-                    
-                case 1: mid++;
-                    break;
-                    
-                case 2: swap(nums[mid],nums[last--]);
-                    break;
-            }
-        }
+      sort(nums.begin(),nums.end());
     }
 };
