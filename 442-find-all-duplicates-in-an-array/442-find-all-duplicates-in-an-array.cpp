55cd085bf534b98a4322cf2c89cf12416ac882bd
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-11 16:19:38 +0530

    Time: 82 ms (54.50%), Space: 33.5 MB (79.87%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -1,67 +1,14 @@
-//--------------------------This solution gives me TLE---------------------------------
-// class Solution {
-// public:
-//     vector<int> findDuplicates(vector<int>& nums) {
-        
-//         if(nums.empty())
-//             return {};
-        
-//         vector<int>answer;
-        
-//         for(int i=0;i<nums.size();++i)
-//         {
-//             for(int j=i+1;j<nums.size();++j)
-//             {
-//                 if(nums[i] != nums[j])
-//                     continue;
-                
-//                 else
-//                 {
-//                     answer.push_back(nums[i]);
-//                     break;
-//                 }
-//             }
-//         }
-       
-//         return answer;
-        
-//     }
-// };
-
-
-
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        
-        vector<int>answer;
-        int n = nums.size();
-        
-        for(int i=0;i<n;++i)
-        {
-            int num = abs(nums[i]);
-            
-            if(nums[num-1]<0)
+        int n=nums.size();
+            vector<int>v1;
+       sort(nums.begin(),nums.end());
+            for(int i=0;i<n-1;++i)
             {
-                answer.push_back(num);
+                    if(nums[i]==nums[i+1])
+                            v1.push_back(nums[i]);
             }
-            else
-            {
-                nums[num-1] *= -1;
-            }
-        }
-        
-        return answer;
-        
+            return v1;
     }
 };
-
-
-
-
-
-
-
-
-
-
