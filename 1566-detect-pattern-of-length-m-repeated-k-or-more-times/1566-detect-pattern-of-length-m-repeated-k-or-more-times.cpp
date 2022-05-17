1dc5d5fbce2d9fe32b927f0c3b2da1827b2fa119
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-17 11:00:27 +0530

    Time: 4 ms (55.41%), Space: 8.2 MB (52.38%) - LeetHub

---
+++ 1566-detect-pattern-of-length-m-repeated-k-or-more-times/1566-detect-pattern-of-length-m-repeated-k-or-more-times.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsPattern(vector<int>& arr, int m, int k) {
+            int count=0;
+            for(int i=0;i+m<arr.size();++i)
+            {
+                    if(arr[i]!=arr[i+m])count=0;
+                    count+=(arr[i]==arr[i+m]);
+                    if(count==(k-1)*m)return true;
+            }
+            return false;
+    }
+};
+
