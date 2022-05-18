bb6432c8f1fb67b26d3fd518a7dca8ffdd6ab8f5
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-18 18:14:19 +0530

    Time: 1597 ms (5.06%), Space: 184 MB (42.23%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,53 @@
+static int speedUp=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+        
+        unordered_map<int,vector<int>>adj;//ADJ list
+        void dfs(int u,vector<int>& disc,vector<int>& low,vector<int>& parent,vector<vector<int>>& bridges)
+        {
+                static int time=0;
+                disc[u]=low[u]=time;
+                time+=1;
+                
+                for(int v : adj[u])
+                {
+                        if(disc[v]==-1)//IF v is not visited initially -1
+                        {
+                                parent[v]=u;
+                                dfs(v,disc,low,parent,bridges);
+                                low[u]=min(low[u],low[v]);
+                                
+                                if(low[v]>disc[u]){bridges.push_back(vector<int>({u,v}));}
+                        }
+                        else if(v!=parent[u])//Check the previous / back edge
+                                low[u]=min(low[u],disc[v]);
+                }
+        }
+        
+        //Using tarjan's ALGO
+        void findbridgesTarjan(int V,vector<vector<int>>& bridges)
+        {
+                vector<int>disc(V,-1),low(V,-1),parent(V,-1);
+                //Using dfs for each component
+                for(int i=0;i<V;++i)
+                        if(disc[i]==-1){dfs(i,disc,low,parent,bridges);}
+        }
+        
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        for(int i=0;i<connections.size();++i)//Make adj list
+        {
+                adj[connections[i][0]].push_back(connections[i][1]);
+                adj[connections[i][1]].push_back(connections[i][0]);
+        }
+            
+            vector<vector<int>>bridges; //Stroe all the bridges as a pairs
+            findbridgesTarjan(n,bridges);
+            return bridges;
+    }
+};
