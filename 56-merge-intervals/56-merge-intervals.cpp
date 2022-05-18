1d80ee4c72ad2b0c6d1ab61fe9f7c643535cc606
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-18 14:15:58 +0530

    Time: 47 ms (60.73%), Space: 20 MB (27.65%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -7,12 +7,9 @@ public:
             sort(intervals.begin(),intervals.end());
             for(int i=1;i<intervals.size();i++)
             {
-                    int l=max(intervals[i][1],intervals[i][0]);
-                    int r=min(intervals[i-1][1],intervals[i-1][0]);
-                    int l1=max(intervals[i-1][0],intervals[i-1][1]);
-                    int r1=min(intervals[i][0],intervals[i][1]);
-                    l=max(l,l1);
-                    r=min(r,r1);
+                    int l=max(intervals[i-1][1],intervals[i][1]);
+                    int r=min(intervals[i-1][0],intervals[i][0]);
+                 
                     
                     if(intervals[i][0] <= intervals[i-1][1])
                     {
