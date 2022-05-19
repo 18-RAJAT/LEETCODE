9c12d46c9c4e35b7fb7a90fc33f5ac0e3d4c5b2e
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-19 17:35:13 +0530

    Time: 29 ms (66.79%), Space: 12.8 MB (62.45%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        if(s.length()<=1){return s.length();}
+            int left=0,right=s.length()-1;
+            while(left<right and s[left]==s[right])
+            {
+                    char ch=s[left];
+                    while(left<=right and s[left]==ch)++left;
+                    while(left<=right and s[right]==ch)--right;
+            }
+           return right-left+1;
+    }
+};
