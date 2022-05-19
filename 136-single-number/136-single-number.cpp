18126b6f346e1cffe31bccc1b7c3e90b0233b6ad
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-19 15:25:02 +0530

    Time: 38 ms (26.02%), Space: 16.9 MB (86.82%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int target=0;
+            for(int i=0;i<nums.size();++i)
+            {
+                    target=target^nums[i];
+            }
+            return target;
+    }
+};
