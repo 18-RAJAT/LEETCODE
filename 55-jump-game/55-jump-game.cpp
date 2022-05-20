2a41810f896a5bdc82c3dcae7263e320a2afdc4e
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-20 14:24:33 +0530

    Time: 65 ms (87.85%), Space: 48.3 MB (93.02%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,35 @@
+#define ll long long
+#define f(a,b) for(int i=a;i<b;i++)
+#define f2(x,a,b) for(int x=a;x<b;x++)
+#define fr(b,a) for(int i=b;i>=a;i--)
+#define fit(nums) for(auto &it:nums)
+#define v vector
+#define p priority_queue
+#define um unordered_map
+#define us unordered_set
+#define pb push_back
+#define ppb pop_back
+#define TN TreeNode
+#define LN ListNode
+
+static int fastio = []() {
+    #define endl '\n'
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+            int n=nums.size();
+            int res=0;
+            for(int i=0;i<n;++i)
+            {
+                    if(i>res){return false;}
+                    res=max(i+nums[i],res);
+            }
+            return true;
+    }
+};
