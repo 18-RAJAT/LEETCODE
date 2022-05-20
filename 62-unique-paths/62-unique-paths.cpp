d9b257e9473d6a85ccf4100bfa883ed0a2203844
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-20 08:17:23 +0530

    Time: 0 ms (100.00%), Space: 6 MB (61.68%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -22,22 +22,15 @@ static int fastio = []() {
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
-         int dp[n][m];
-         for(int i=0;i<n;++i)
-         {
-                 for(int j=0;j<m;++j)
-                 {
-                      if(i==0 or j==0){dp[i][j]=1;}
-                      else{dp[i][j]=dp[i-1][j]+dp[i][j-1];}
-                 }
-         }
+        int dp[n][m];
+            for(int i=0;i<n;++i)
+            {
+                    for(int j=0;j<m;++j)
+                    {
+                            if(i==0 or j==0){dp[i][j]=1;}
+                            else{dp[i][j]=dp[i-1][j]+dp[i][j-1];}
+                    }
+            }
             return dp[n-1][m-1];
     }
 };
-
-
-
-
-
-
