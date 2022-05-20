d41c52029623f05b871a7521ebc10f744468c960
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-20 14:28:47 +0530

    Time: 4 ms (41.69%), Space: 8.7 MB (73.95%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -4,31 +4,8 @@ public:
             int temp=0;
             for(int i=0;i<nums.size();++i)
             {
-                  if(nums[i]!=val){swap(nums[i],nums[temp]);temp=temp+1;}
+                    if(nums[i]!=val){swap(nums[i],nums[temp]);temp+=1;}
             }
             return temp;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
