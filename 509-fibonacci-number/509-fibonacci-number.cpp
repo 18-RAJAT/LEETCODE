c8e7ca89dc7f0f69ecdd459f8f7192bb517b4eef
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-25 18:45:55 +0530

    Time: 3 ms (58.43%), Space: 6 MB (42.29%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,16 +1,28 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//             //USING RECURSION
+//             if(n==0 or n==1){return n;}
+//             int subproblem1=fib(n-1);
+//             int subproblem2=fib(n-2);
+//             return subproblem1+subproblem2;
+//     }
+// };
+
+
+
+
+//USING DP
 class Solution {
 public:
     int fib(int n) {
-        
-        if(n == 0 or n == 1)
+            if(n<2){return n;}
+            int dp[n+1];
+            dp[0]=0,dp[1]=1;
+        for(int i=2;i<=n;++i)
         {
-            return n;
+                dp[i]=dp[i-1]+dp[i-2];
         }
-        
-        int subproblem1 = fib(n-1);
-        int subproblem2 = fib(n-2);
-        
-        return subproblem1 + subproblem2;
-        
+            return dp[n];
     }
 };
