7fbee694b807a785d587ec0dda4d017e1ce3f0d4
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-05-25 16:10:15 +0530

    Time: 3 ms (44.71%), Space: 6.3 MB (7.56%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+     if(n==0 and n==1){return false;}
+            vector<int>dp(n+1,0);
+            for(int i=2;i<=n;++i)
+            {
+                    for(int j=1;j<=sqrt(i);++j)
+                    {
+                            if(i%j==0 and dp[i-j]==0){dp[i]=2;}
+                    }
+            }
+            if(dp[n]==2){return true;}
+            return false;
+    }
+};
