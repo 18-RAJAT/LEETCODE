142cbe19200996d52006a2f13281f996d653cb85
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-06-03 21:41:10 +0530

    Time: 5 ms (82.69%), Space: 9.1 MB (55.01%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
+    long max1 = LONG_MIN;
+    long max2 = LONG_MIN;
+    long max3 = LONG_MIN;
 
-        map<int,bool>m;
-        for(auto i : nums)
-        {
-            m[i] = true;
-        }
-        nums = {};
-        
-        for(auto i : m)
-        
-            nums.push_back(i.first);
-          int n = nums.size();
-        return (n < 3) ? nums[n-1] : nums[n-3];
+    for (auto num : nums)
+      if (num > max1) 
+      {
+        max3 = max2;
+        max2 = max1;
+        max1 = num;
+      } 
+            
+        else if (max1 > num && num > max2)
+      {
+        max3 = max2;
+        max2 = num;
+      } 
+            
+        else if (max2 > num && num > max3) 
+      {
+        max3 = num;
+      }
+
+    return max3 == LONG_MIN ? max1 : max3;
     }
 };
