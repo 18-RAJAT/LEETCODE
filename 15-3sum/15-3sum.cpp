a5affaac66cf0b217b61ba2ca07f6d070b8f3b33
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-06-05 14:19:06 +0530

    Time: 146 ms (43.76%), Space: 20.1 MB (68.29%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,48 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-         vector<vector<int>>answer;
-        
-        sort(nums.begin(),nums.end());
-        
-        for(int i=0;i<nums.size();++i)
-        {
-            int target = -nums[i];
+            if(nums.size()<3){return {};}
+            vector<vector<int>>answer;
+            sort(nums.begin(),nums.end());
             
-            //Starting position
-            int left = i+1;
-            //Ending position
-            int right = nums.size() - 1;
-            
-            while(left < right)
+            for(int i=0;i+2<nums.size();++i)
             {
-                int sum = nums[left] + nums[right];
-                
-                // Finding answer which start from number num[i]
-                if(sum < target)
-                    left++;
-                
-                else if(sum > target)
-                    right--;
-                
-                else
-                {
-                    vector<int>triplet = {nums[i],nums[left],nums[right]};
+                    if(i>0 and nums[i]==nums[i-1]){continue;}
+                    int l=i+1;
+                    int r=nums.size()-1;
                     
-                    answer.push_back(triplet);
+                    while(l<r)
+                    {
+                    int sum=nums[i]+nums[r]+nums[l];
                     
-                    while(left < right and nums[left] == triplet[1])
-                        left++;
-                    
-                    while(left < right and nums[left] == triplet[2])
-                        right--;
-                }
+                    if(sum==0)
+                    {
+                            answer.push_back({nums[i],nums[l++],nums[r--]});
+                            
+                            while(l<r and nums[l]==nums[l-1]){l++;}
+                            while(l<r and nums[r]==nums[r+1]){r--;}
+                    }
+                            else if(sum<0){l++;}
+                            else{r--;}
+                    }       
             }
-            while(i+1 < nums.size() and nums[i+1] == nums[i])
-                i++;
-        }
-        
-         return answer;
+            return answer;
     }
 };
