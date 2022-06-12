aa83380639fb44c18c23e3181024d9858ec012ea
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-06-13 03:16:15 +0530

    Time: 187 ms (12.04%), Space: 54.8 MB (6.47%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       set<int>s;
+            for(int i=0;i<nums.size();++i)s.insert(nums[i]);
+            if(nums.size()!=s.size()){return true;}
+            return false;
+    }
+};
