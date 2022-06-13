3ab51b71f6d371142a3d6e3a79e6afc1e7c7cdcc
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-06-13 14:19:08 +0530

    Time: 47 ms (18.64%), Space: 24.1 MB (56.95%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,17 +1,39 @@
+#define ll long long
+#define f(a,b) for(int i=a;i<b;i++)
+#define f2(x,a,b) for(int x=a;x<b;x++)
+#define fr(b,a) for(int i=b;i>=a;i--)
+#define fit(nums) for(auto &it:nums)
+#define v vector
+#define p priority_queue
+#define um unordered_map
+#define us unordered_set
+#define pb push_back
+#define ppb pop_back
+#define TN TreeNode
+#define LN ListNode
+#define vi vector<int>ans
+
+static int fastio = []() {
+    #define endl '\n'
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(0);
+    return 0;
+}();
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n=nums.size();
-            vector<int>answer(n,1);
-            for(int i=1;i<nums.size();++i)
-                    answer[i]=answer[i-1]*nums[i-1];
-            
-            int suffix=1;
+     int n=nums.size();
+            vi(n,1);
+            f(1,n){ans[i]=ans[i-1]*nums[i-1];}
+            int prev=1;
             for(int i=n-1;i>=0;--i)
             {
-                    answer[i]=answer[i]*suffix;
-                    suffix=suffix*nums[i];
+                    ans[i]*=prev;
+                    prev*=nums[i];
+                    // cout<<"Std";
             }
-            return answer;
+            return ans;
     }
 };
