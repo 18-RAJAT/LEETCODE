55e471048c997302b6d3cd049774a32f3385c6b9
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-06-14 02:18:20 +0530

    Time: 3 ms (77.52%), Space: 6.8 MB (12.45%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cpp
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         unordered_map<char,int>mp;
-        
         for(int i = 0; i < s.size(); i++) mp[s[i]]++;      // step 1
-        
         int ans = 0;
         vector<int>odds;
         for(auto i : mp)
@@ -12,7 +10,6 @@ public:
             if(i.second % 2 != 0) odds.push_back(i.second);
             else ans += (i.second);
         }
-        
         if(odds.size() != 0)
         {                            // step 3
             ans += odds[0];
