f7a8659fb085f6bd7340b95ffffe01456df36cdc
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-06-16 17:06:47 +0530

    Time: 4 ms (33.45%), Space: 6.5 MB (43.03%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int minimumMoves(string s) {
-           int answer=0;
-            for(int i=0;i<s.size();++i) 
-                    if(s[i]=='X')answer++,i=i+2;
-            return answer;
+                int ct=0;
+            for(int i=0;i<s.length();++i)
+            {
+                    if(s[i]=='X'){ct++;i=i+2;}
+            }
+            return ct;
     }
 };
