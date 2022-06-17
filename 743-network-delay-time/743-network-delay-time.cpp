48e5877a48a02a9fa155927b9b24629bc4d82569
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-06-17 09:23:54 +0530

    Time: 337 ms (13.13%), Space: 36.5 MB (99.59%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,36 +1,29 @@
-// 1st Approach Using [ Bellman Ford ]
-
-// Bellman Ford(Time Complexity) - VE
-
+#define imax INT_MAX
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        
-        
-        vector<int>dis(n+1,INT_MAX);
-        dis[k]=0;
-        for(int i=1;i<n;i++)
-           {
-             for(int j=0;j<times.size();j++)
-                 {
-                   int u=times[j][0];
-                   int v=times[j][1];
-                   int w=times[j][2];
-                 
-                   if(dis[u]!=INT_MAX && dis[u]+w<dis[v])
-                   {
-                     dis[v]=w+dis[u];
-                   }
-                }
-           }
-		   
-        int ans=0;
-        for(int i=1;i<=n;i++)
-           {
-            if(dis[i]==INT_MAX) return -1;    // If any node is not reachable from k
-            ans=max(ans,dis[i]);
-           }
-        return ans;
-        
+            // Using bellman Ford
+        vector<int>dis(n+1,imax);
+            dis[k]=0;
+            for(int i=1;i<n;++i)
+            {
+                    for(int j=0;j<times.size();++j)
+                    {
+                            int u=times[j][0];
+                            int v=times[j][1];
+                            int w=times[j][2];
+                            if(dis[u]!=imax and dis[u]+w<dis[v])
+                            {
+                                    dis[v]=w+dis[u];
+                            }
+                    }
+            }
+            int answer=0;
+            for(int i=1;i<=n;++i)
+            {
+                         if(dis[i]==imax){return -1;}
+                         answer=max(answer,dis[i]);
+            }
+            return answer;
     }
 };
