5928d871a0577595188efc3303779d575106e4ec
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-07-07 18:10:00 +0530

    Time: 9 ms (47.23%), Space: 6.3 MB (89.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,3 +1,32 @@
+// class Solution {
+// public:
+//     bool helper(string &s1,string &s2,string &s3,int i,int j,int k)
+//     {
+//         if(s1[i]!=s3[k] and s2[j]!=s3[k]){return false;}
+//         if(i>=s1.length() and j>=s2.length()){return true;}
+//         if(k==s3.length()){return true;}
+        
+//          int x=0,y=0,z=0;
+//         if(s1[i]==s3[k] and s2[k]!=s3[k])
+//         {x=helper(s1,s2,s3,i+1,j,k+1);}
+        
+//         if(s2[j]==s3[k] and s1[k]!=s3[k])
+//         {y=helper(s1,s2,s3,i,j+1,k+1);}
+        
+//         if(s1[i]==s3[k] and s2[k]==s3[k])
+//         {z=helper(s1,s2,s3,i+1,j,k+1) or helper(s1,s2,s3,i,j+1,k+1);}
+//         return x or y or z;
+//     }
+//     bool isInterleave(string s1, string s2, string s3) {
+//         int p=s1.length(),q=s2.length(),r=s3.length();
+//         if(p+q!=r){return false;}
+//         return helper(s1,s2,s3,0,0,0);
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
