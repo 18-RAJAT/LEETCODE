ca59e83b708ad9243ae9ccfa2880224e1927c18c
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-07-31 21:19:11 +0530

    Time: 773 ms (33.10%), Space: 150 MB (92.96%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -5,9 +5,7 @@ public:
     NumArray(vector<int>& nums) {
         v=nums;
         sum=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-        }
+        for(int i=0;i<nums.size();i++){sum+=nums[i];}
     }
     
     void update(int index, int val) {
