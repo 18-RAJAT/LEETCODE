6b8c8d1cc04746ca573ed50ef29d09450909b07e
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-08-08 21:35:11 +0530

    Time: 36 ms (61.44%), Space: 31.2 MB (53.59%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,121 @@
+// TLE
+
+
+// #define MP make_pair
+// #define PB push_back
+// #define INF (int)1e9
+// #define EPS 1e-9
+// #define MOD 1000000007
+// #define PI 3.1415926535897932384626433832795
+// #define read(type) readInt<type>()
+// const double pi=acos(-1.0);
+// #define ll long long
+// typedef pair<int, int> PII;
+// typedef vector<int> VI;
+// typedef vector<vector<int>> VII;
+// typedef vector<string> VS;
+// typedef vector<vector<string>> VVS;
+// typedef vector<PII> VIII;
+// typedef vector<VI> VVI;
+// typedef map<int,int> MPII;
+// typedef priority_queue<pair<int,pair<int,int>>> PQVI;
+// typedef unordered_map<string,vector<string>> MPSVS;
+// typedef set<int> SETI;
+// typedef multiset<int> MSETI;
+// typedef long int int32;
+// typedef unsigned long int uint32;
+// typedef long long int int64;
+// typedef unsigned long long int  uint64;
+// #define all(x) sort(x.begin(), x.end())
+// #define ff(a,b) for(int i=a;i<b;i++)
+// #define f1(i,s,e) for(long long int i=s;i<e;i++)
+// #define ff1(pass) for(long long int i=n-2;i>=0;--i)
+// #define cf(i,s,e) for(long long int i=s;i<=e;i++)
+// #define FOREACH(n) for(auto it:n)
+// #define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+// #define pb push_back
+// #define eb emplace_back
+
+
+// class Solution {
+// public:
+//     int pivotIndex(vector<int>& nums) {
+//        if(nums.empty()){return -1;}
+//         if(nums.size()==1){return 0;}
+//         f1(i,0,nums.size())
+//         {
+//             int left=0,right=0;
+           
+            
+//             f1(j,0,i)
+//                 left=left+nums[j];
+            
+            
+//             f1(j,i+1,nums.size())
+//                 right=right+nums[j];
+            
+//             if(right==left){return i;}
+//         }
+//         return -1;
+//     }
+// };
+
+
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+#define ll long long
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<vector<int>> VII;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<PII> VIII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef priority_queue<pair<int,pair<int,int>>> PQVI;
+typedef unordered_map<string,vector<string>> MPSVS;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+#define all(x) sort(x.begin(), x.end())
+#define ff(a,b) for(int i=a;i<b;i++)
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define ff1(pass) for(long long int i=n-2;i>=0;--i)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define FOREACH(n) for(auto it:n)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+       if(nums.empty()){return -1;}
+        if(nums.size()==1){return 0;}
+        int left=0,right=0;
+           
+        f1(i,0,nums.size())
+            right+=nums[i];
+        
+        f1(i,0,nums.size())
+        {
+            right-=nums[i];
+            
+            if(left==right)return i;
+            
+            left+=nums[i];
+        }
+        return -1;
+    }
+};
