4144ff1f4193c5f33fbfc2cf7a25b2158046edef
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-08-18 10:31:12 +0530

    Time: 467 ms (21.54%), Space: 80.2 MB (39.23%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,56 @@
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+#define ll long long
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<vector<int>> VII;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<PII> VIII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef priority_queue<int>PQII;
+typedef unordered_map<string,vector<string>> MPSVS;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+#define all(x) sort(x.begin(), x.end())
+#define ff(a,b) for(int i=a;i<b;i++)
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define ff1(pass) for(long long int i=n-2;i>=0;--i)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define FOREACH(n) for(auto it:n)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+
+
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        
+        MPII mp;
+        for(auto it:arr){mp[it]++;}
+        
+        PQII q;
+        for(auto x:mp){q.push(x.second);}
+        
+        int n=arr.size();
+        int result=0,sum=0;
+        while(sum<ceil(n/2))
+        {
+            sum=sum+q.top();q.pop();result++;
+        }
+        return result;
+    }
+};
