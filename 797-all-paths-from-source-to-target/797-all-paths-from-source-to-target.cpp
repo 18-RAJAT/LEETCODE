0c13a7a03081797e44725dd5d885fd833cda8fec
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-08-19 12:08:06 +0530

    Time: 14 ms (85.37%), Space: 10.7 MB (66.97%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,34 +1,59 @@
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
 #define ll long long
-typedef vector<pair<int,int>> vv;
 typedef pair<int, int> PII;
-typedef pair<int,int> PAIR;
 typedef vector<int> VI;
 typedef vector<vector<int>> VII;
 typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
 typedef vector<PII> VIII;
 typedef vector<VI> VVI;
 typedef map<int,int> MPII;
-typedef unordered_map<char,int> MPC;
-typedef map<int,vector<int>> mvi;
+typedef priority_queue<pair<int,pair<int,int>>> PQVI;
+typedef unordered_map<string,vector<string>> MPSVS;
 typedef set<int> SETI;
 typedef multiset<int> MSETI;
-#define ff(a,b) for(int i=0;i<n;++i)
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+#define all(x) sort(x.begin(), x.end())
+#define ff(a,b) for(int i=a;i<b;i++)
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define ff1(pass) for(long long int i=n-2;i>=0;--i)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define FOREACH(n) for(auto it:n)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
 #define pb push_back
-#define PB pop_back
+#define eb emplace_back
+
+
 
 class Solution {
 public:
-    void dfs(VII&graph,VII&answer,VI&adj,int u)
+    void dfs(VII& graph,VII& answer,VI& adj,int u)
     {
-        adj.pb(u);
-        if(u==graph.size()-1){answer.pb(adj);}
+        adj.PB(u);
+        if(u==graph.size()-1)
+        {answer.PB(adj);}
         else
+        {
             for(auto it:graph[u])
-            dfs(graph,answer,adj,it);
-            adj.PB();
+            {
+                dfs(graph,answer,adj,it);adj.pop_back();
+            }
+        }
     }
-    vector<vector<int>> allPathsSourceTarget(VII& graph) {
-        VII answer;VI adj;
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        VII answer;
+        VI adj;
         dfs(graph,answer,adj,0);
         return answer;
     }
