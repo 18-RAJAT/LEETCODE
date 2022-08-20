e237d3304a58ef4a62987fb668a3450372bc243a
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-08-20 18:59:11 +0530

    Time: 145 ms (14.63%), Space: 16.2 MB (51.25%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,21 +1,51 @@
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+#define ll long long
+typedef pair<int, int> PII;
+typedef vector<long> VI;
+typedef vector<vector<int>> VII;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<PII> VIII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef priority_queue<pair<int,pair<int,int>>> PQVI;
+typedef unordered_map<string,vector<string>> MPSVS;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+#define all(x) sort(x.begin(), x.end())
+#define ff(a,b) for(int i=a;i<b;i++)
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define ff1(pass) for(long long int j=i+1;j>0;--j)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define FOREACH(n) for(auto it:n)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
 class Solution {
-public:
-    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-      priority_queue<int> pq{};
-        int minStops = 0, currentStationIndex = 0, totalStations = stations.size();
-        while(startFuel < target)
-        {
-            while(currentStationIndex < totalStations && stations[currentStationIndex][0] <= startFuel)
-            {
-                pq.push(stations[currentStationIndex][1]);
-                currentStationIndex++;
-            }
-            if(pq.empty())
-                return -1;
-            startFuel += pq.top();
-            pq.pop();
-            minStops++;
-        }
-        return minStops;
-    }
+ public:
+  int minRefuelStops(int target, int sf, vector<vector<int>>& stations) {
+    int n=stations.size();
+      VI dp(n+1);
+      dp[0]=sf;
+      f1(i,0,n)
+       ff1(pass)
+          if(dp[j-1]>=stations[i][0]){dp[j]=max(dp[j],dp[j-1]+stations[i][1]);}
+      
+            f1(i,0,dp.size())
+          if(dp[i]>=target)return i;
+        
+      return -1;
+  }
 };
