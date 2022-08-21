f84f87ee6eded2267364cc35bec9aaa3584450d7
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-08-21 20:16:57 +0530

    Time: 8 ms (95.08%), Space: 7.4 MB (76.23%) - LeetHub

--- 936-stamping-the-sequence/936-stamping-the-sequence.cpp
+++ 936-stamping-the-sequence/936-stamping-the-sequence.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
     vector<int> movesToStamp(string stamp, string target) {
-        int s=stamp.size(),t=target.size();
-        int count=0;
-        vector<int>ans;
-        bool isSafe=true;
-        while(isSafe){
-            isSafe=false;
-            for(int i=0;i<=t-s;i++){
-                bool notMatch=true;
-                int dot=0;
-                for(int j=0;j<s;j++){
-                    if(target[i+j]=='.'){
-                        dot++;
-                    }
-                    if(target[i+j]!='.'&&target[i+j]!=stamp[j]){
-                        notMatch=false;
-                        break;
-                    }
-                }
-                if(dot<s&&notMatch){
+         if (stamp == target) return {0};
+        int slen = stamp.size(), tlen = target.size() - slen + 1, i, j;
+        vector<int> ans;
+        bool tdiff = true, sdiff;
+        while (tdiff)
+            for (i = 0, tdiff = false; i < tlen; i++) {
+                for (j = 0, sdiff = false; j < slen; j++)
+                    if (target[i+j] == '*') continue;
+                    else if (target[i+j] != stamp[j]) break;
+                    else sdiff = true;
+                if (j == slen && sdiff) {
+                    for (j = i, tdiff = true; j < slen + i; j++)
+                        target[j] = '*';
                     ans.push_back(i);
-                    isSafe=true;
-                    for(int j=0;j<s;j++){
-                        target[i+j]='.';
-                    }
                 }
             }
-        }
-        for(int i=0;i<t;i++){
-            if(target[i]!='.'){
-                return {};
-            }
-        }
-        reverse(ans.begin(),ans.end());
+        for (i = 0; i < target.size(); i++) if (target[i] != '*') return {};
+        reverse(ans.begin(), ans.end());
         return ans;
+        
     }
 };
