ce457ec7dee14ea0b3265d10cece790b0791163d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-08-26 19:16:01 +0530

    Time: 3 ms (73.16%), Space: 5.9 MB (96.61%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,17 +1,54 @@
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+#define ll long long
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<vector<int>> VII;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<PII> VIII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef priority_queue<pair<int,pair<int,int>>> PQVI;
+typedef unordered_map<string,vector<string>> MPSVS;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+#define all(x) sort(x.begin(), x.end())
+#define ff(a,b) for(int i=a;i<b;i++)
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define ff1(pass) for(long long int i=n-2;i>=0;--i)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define FOREACH(n) for(auto it:n)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+
+
 class Solution {
 public:
+    string sortedNum(int n)
+    {
+        string result=to_string(n);
+        sort(result.begin(),result.end());
+        return result;
+    }
     bool reorderedPowerOf2(int n) {
-        unordered_set<string> st;
-        for(int i=0; i<30; i++) {
-            int num = pow(2, i);
-            string s = to_string(num);
-            sort(s.begin(), s.end());
-            st.insert(s);
+       string str=sortedNum(n);
+        f1(i,0,32)
+        {
+            if(str==sortedNum(1<<i))return true;
         }
-        
-        string s = to_string(n);
-        sort(s.begin(), s.end());
-        
-        return st.find(s) == st.end() ? false : true;
+        return false;
     }
 };
