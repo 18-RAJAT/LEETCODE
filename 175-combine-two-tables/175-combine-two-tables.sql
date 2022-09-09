a1df3058ec7e7ab652d95eb6ef6baced309352cd
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-09-09 14:58:23 +0530

    Time: 742 ms (38.64%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -1,5 +1,7 @@
 /* Write your PL/SQL query statement below */
 
+/* Write your PL/SQL query statement below */
+
 select firstname,lastname,city,state
 from address,person
 where person.personid=address.personid(+)
