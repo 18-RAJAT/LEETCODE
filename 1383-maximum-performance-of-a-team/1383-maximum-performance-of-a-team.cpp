c43be80f13f48425c5186bcbaa3f14734062c824
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-09-11 14:34:04 +0530

    Time: 675 ms (5.10%), Space: 52 MB (8.80%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-       vector<pair<int,int>> v;
-        
-       for(int i=0;i<n;i++) v.push_back({efficiency[i],speed[i]});
-       sort(v.rbegin(),v.rend());
-        
-       long long ans = 0,spd = 0;
-       priority_queue<int,vector<int>,greater<int> >pq;
-        
-       for(int i=0;i<n;i++){
-           if(pq.size() == k){
-               spd -= pq.top(); // remove minimum speed 
-               pq.pop();
-           }
-           
-           spd += v[i].second;
-           pq.push(v[i].second);
-           
-           ans = max(ans,spd * 1LL * v[i].first);
-       }
-        return ans % mod;
+        priority_queue<vector<int>>pq;
+        for(int i=0;i<n;++i)
+            pq.push({efficiency[i],speed[i]});
+			
+        long long cur=0,res=0,mod=pow(10,9)+7;
+        priority_queue<int>topSpeed;
+        for(int i=0;i<n;++i)
+        {
+            auto top = pq.top();
+            cur += top[1]; pq.pop();
+            topSpeed.push(-top[1]);
+			
+            if(topSpeed.size()>k)
+            {
+                cur+=topSpeed.top();
+                topSpeed.pop();
+            }
+            res=max(cur*top[0],res);
+        }
+        return res%mod;
     }
 };
