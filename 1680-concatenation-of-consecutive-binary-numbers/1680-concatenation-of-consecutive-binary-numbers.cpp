19345f18538442c563fdc094c272dee45cb2b358
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-09-23 08:54:08 +0530

    Time: 40 ms (86.96%), Space: 6 MB (47.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,12 +1,29 @@
+#define ll long long int 
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define ff(a,b) for(int i=a;i<b;i++)
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define ff1(pass) for(long long int i=n-2;i>=0;--i)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+
+
 class Solution {
 public:
     int concatenatedBinary(int n) {
-          long ans = 0, mod = 1e9+7, length = 0;
-        for (int i = 1; i <= n; ++i) {
-            
-            if ((i & (i - 1)) == 0) length++;
-            ans = ((ans << length) + i) % mod;
+        ll answer=0;
+        // if(n==1){return n;}
+        // return (n*n)%MOD;
+        ll current=0;
+        cf(i,1,n)
+        {
+            if((i&(i-1))==0)current++;
+            answer=(answer<<current)%MOD;
+            answer+=i%MOD;
         }
-        return ans;
+        return answer;
     }
 };
