bea1e6f14de8f3b3cb074a762a17009c2e691d71
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-09-27 13:44:54 +0530

    Time: 85 ms (41.07%), Space: 12.8 MB (88.39%) - LeetHub

---
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -0,0 +1,75 @@
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+#define ll long long
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<vector<int>> VII;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<PII> VIII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef priority_queue<pair<int,pair<int,int>>> PQVI;
+typedef unordered_map<string,vector<string>> MPSVS;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+#define all(x) sort(x.begin(), x.end())
+#define ff(a,b) for(int i=a;i<b;i++)
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define ff1(pass) for(long long int i=n-2;i>=0;--i)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define FOREACH(n) for(auto it:n)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        int right=-1;
+        int n=dominoes.size();
+        f1(i,0,n)
+        {
+            if(dominoes[i]=='L')
+            {
+                if(right==-1)
+                {
+                    for(int j=i-1;j>=0 and dominoes[j]=='.';--j){dominoes[j]='L';}
+                }
+                else
+                {
+                    for(int j=right+1,k=i-1;j<k;++j,--k)
+                    {
+                        dominoes[j]='R';
+                        dominoes[k]='L';
+                    }
+                    right=-1;
+                }
+            }
+            else if(dominoes[i]=='R')
+            {
+                if(right!=-1)
+                {
+                    for(int j=right+1;j<i;++j){dominoes[j]='R';}
+                }right=i;
+            }
+        }
+        
+        if(right!=-1)
+        {
+            for(int j=right+1;j<n;++j){dominoes[j]='R';}
+        }
+        return dominoes;
+    }
+};
