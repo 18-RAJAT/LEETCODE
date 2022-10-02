7085112ea22e77b8d979d94bd0a343a16ed3ebf0
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-10-02 22:52:45 +0530

    Time: 23 ms (85.42%), Space: 6 MB (96.28%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,23 +1,46 @@
+// class Solution {
+// public:
+//     int solve(int n,int k,int target)
+//     {
+        
+//         if(target<0) return 0;
+//         if(target==0 and n!=0) return 0;
+//         if(target!=0 and n==0) return 0;
+//         if(target==0 and n==0) return 1;
+       
+//         int ans=0;
+//         for(int i=1;i<=k;++i)
+//         {
+//             ans=(ans+solve(n-1,k,target-i))%1000000007;
+//         }
+//         return ans;
+//     }
+//     int numRollsToTarget(int n, int k, int target) 
+//     {
+//         return solve(n,k,target);
+//     }
+// };
+
+
 class Solution {
 public:
-    int numRollsToTarget(int n, int k, int target) {
-        int dp[n+1][target+1];
-        memset(dp,0,sizeof(dp));
-        dp[0][0]=1;
-        for(int i=1;i<=n;i++)
+    int dp[31][1001];
+    int solve(int n,int k,int target)
+    {
+        
+        if(target<0) return 0;
+        if(target==0 and n!=0 or target!=0 and n==0) return 0;
+        if(target==0 and n==0) return 1;
+        if(dp[n][target]!=-1) return dp[n][target];
+        int ans=0;
+        for(int i=1;i<=k;++i)
         {
-            for(int j=1;j<=target;j++)
-            {
-                for(int l=1;l<=k;l++)
-                {
-                    if(j-l>=0)
-                    {
-                        dp[i][j]+=dp[i-1][j-l];
-                        dp[i][j]%=1000000007;
-                    }
-                }
-            }
+            ans=(ans+solve(n-1,k,target-i))%1000000007;
         }
-        return dp[n][target];      
+        return dp[n][target]=ans;
+    }
+    int numRollsToTarget(int n, int k, int target) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n,k,target);
     }
 };
