7c472d0ea496e0e212848ef233d5ed97cfa657cc
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-10-10 19:18:27 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (5.00%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string breakPalindrome(string pali) {
+        int n=pali.size();
+        if(n<2)
+        {
+            return "";
+        }
+        for(int i=0;i<n/2;++i)
+        {
+            if(pali[i]!='a')
+            {
+                pali[i]='a';
+                return pali;
+            }
+        }
+        pali[n-1]='b';
+        return pali;
+    }
+};
