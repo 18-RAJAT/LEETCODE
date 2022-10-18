31a0555cd5fa01c113c7cde0950d089f3db788d1
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-10-19 00:33:50 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (56.81%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     string countAndSay(int n) {
-         if(n==1) return "1";
-        string str=countAndSay(n-1);
-        cout<<str<<endl;
-        string ans="";
-        
-        if(str.length()==1){
-            ans+="1";
-            ans+=str[0];
+        if(n==1)
+        {
+            return "1";
         }
-        
-        if(str.length()>1){
-        for(int i=0;i<str.length();i++){
-            int count=1;
-            while(i<str.length()-1&&str[i]==str[i+1]){
-                count++;
-                i++;
+        auto s=countAndSay(n-1);
+        string ans="";
+        int x=s.size();
+        if(x==1)
+        {
+            ans+="1";
+            ans+=s[0];
+        }
+        if(x>1)
+        {
+            for(int i=0;i<x;++i)
+            {
+                int ct=1;
+                while(i<x-1 and s[i]==s[i+1])
+                {
+                    ct++;i++;
+                }
+                ans+=('0'+ct);
+                ans+=s[i];
             }
-            
-            ans+=(char)('0'+count);
-            ans+=str[i];
-            
-               
-        }}
+        }
         return ans;
     }
 };
