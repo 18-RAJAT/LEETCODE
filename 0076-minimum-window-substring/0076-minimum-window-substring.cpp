979395c1b6d6ccc212812714cde5049b8b7d4a7d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-10-22 09:10:55 +0530

    Time: 49 ms (36.56%), Space: 7.9 MB (38.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,33 +1,55 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-         unordered_map<char, int> mp;
-        int n = s.size();
-        for(char i : t) mp[i]++;    // mantain count of each char of t
-        int i = 0, j = 0, mn = INT_MAX;
         
-        int count = mp.size();
-        string ans = "";
+        int n=s.size();
+        int m=t.size();
+        string res="";
         
-        while(j < n){
-            if(mp.find(s[j]) != mp.end()) {     // if that char is found reduce it's count
-                mp[s[j]]--;
-                if(mp[s[j]] == 0) count--;     //if that char's count got to 0 then we've completely found one char so reduce var count
-            }
+        if(n<m)
+        {
+            return res;
+        }
+        map<char,int>patternHash,stringHash;
+        for(auto it:t) patternHash[it]++;
+        
+        int i=0;
+        int j=0;
+        int count=0;
+        int len=INT_MAX;
+        int startIndex=-1;
+        
+        while(j<n)
+        {
+            stringHash[s[j]]++;
             
-            while(count == 0){
-                if(mn > j-i+1){
-                    mn = min(mn, j-i+1);     // change min as found a valid canditate for ans
-                    ans = s.substr(i,j-i+1);
+            if(stringHash[s[j]]<=patternHash[s[j]])
+            {
+                count++;
+            }
+            if(count==m)
+            {                
+                while(stringHash[s[i]]>patternHash[s[i]] or patternHash[s[i]]==0)
+                {
+                    
+                    if(stringHash[s[i]]>patternHash[s[i]])
+                    stringHash[s[i]]--;
+                    i++;
+                    
                 }
-                if(mp.find(s[i]) != mp.end()){    //optimize further and slide the window
-                    mp[s[i]]++;
-                    if(mp[s[i]]==1) count++;
+                int found=j-i+1;
+                if(len>found)
+                {
+                    len=found;
+                    startIndex=i;
                 }
-                i++;
             }
             j++;
         }
-        return ans;
+        if(startIndex==-1)
+        {
+            return "";
+        }
+        return s.substr(startIndex,len);
     }
 };
