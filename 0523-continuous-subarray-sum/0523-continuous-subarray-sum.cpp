9a0d1f2625dac27f00a6f54fcff1a233e340a381
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-10-26 17:03:41 +0530

    Time: 601 ms (14.50%), Space: 115.2 MB (12.56%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,76 @@
+#define MP make_pair
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define ll long long
+#define all(x) sort(x.begin(), x.end())
+#define ff(a,b) for(int i=a;i<b;i++)
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define FOREACH(n) for(auto it:n)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<vector<int>> VII;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<PII> VIII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef priority_queue<pair<int,pair<int,int>>> PQVI;
+typedef unordered_map<string,vector<string>> MPSVS;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        // sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n<2)
+        {
+            return false;
+        }
+        
+        if(k==0)
+        {
+            f1(i,0,n-1)
+            {
+                if(nums[i]==0 and nums[i+1]==0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        MPII mp;
+        mp[0]=-1;
+        int totalSum=0;
+        f1(i,0,n)
+        {
+           totalSum+=nums[i];
+           totalSum%=k;
+            
+            if(mp.find(totalSum)!=mp.end())
+            {
+                if(i-mp[totalSum]>1)
+                {
+                    return true;
+                }
+            }
+                else
+                {
+                    mp[totalSum]=i;
+                }
+        }
+        return false;    
+    }
+};
