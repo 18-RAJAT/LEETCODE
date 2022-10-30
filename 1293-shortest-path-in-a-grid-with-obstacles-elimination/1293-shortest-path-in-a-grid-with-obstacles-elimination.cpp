968dc553047ca24e154580005020ac8bcb1c6094
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-10-30 20:50:13 +0530

    Time: 809 ms (15.63%), Space: 129.5 MB (5.23%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,40 +1,56 @@
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int rows = grid.size(), cols = grid[0].size();
+    
+        vector<vector<int>>directions{{-1,0},{0,-1},{1,0},{0,1}};
+        
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<vector<bool>>>visited(m,vector<vector<bool>>(n,vector<bool>(k+1)));
 
-        int cnt = 0;
-        vector<pair<int, int>> deltas{{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-        vector<vector<int>> vis(rows, vector<int>(cols, -1));
-
-        queue<pair<pair<int, int>, int>> qu; // row, col, k
-        qu.push({{0, 0}, k});
-        while(!qu.empty()) {
-            int sz = qu.size();
-            while(sz--) {
-                auto & [cell, k_left] = qu.front();
-                auto & [row, col] = cell;
-
-                if(row == rows - 1 && col == cols - 1) return cnt;
-
-                for(auto & [dr, dc] : deltas) {
-                    int new_row = row + dr, new_col = col + dc;
-                    if(new_row < 0 || new_row == rows || new_col < 0 || new_col == cols) continue;
-
-                    int new_k = k_left - grid[new_row][new_col];
-                    if(new_k < 0) continue;
-
-                    if(vis[new_row][new_col] >= new_k) continue; // we already have a better path
-
-                    vis[new_row][new_col] = new_k;
-                    qu.push({{new_row, new_col}, new_k});
+        queue<vector<int>>q;
+        q.push({0,0,k});
+        
+        visited[0][0][k]=true;
+        
+        int countSteps=0;
+        
+        while(!q.empty())
+        {
+            int sz=q.size();
+            for(int i=0;i<sz;++i)
+            {
+                auto cur=q.front();
+                q.pop();
+                
+                if(cur[0]==m-1 and cur[1]==n-1) 
+                {
+                    return countSteps;
                 }
 
-                qu.pop();
+                for(auto dir:directions)
+                {
+                    int newRow=dir[0]+cur[0];
+                    int newCol=dir[1]+cur[1];
+                    int obstacle=cur[2];
+                    
+                    if(newRow>=0 and newCol>=0 and newRow<m and newCol<n)
+                    {
+                        
+                        if(grid[newRow][newCol]==0 and !visited[newRow][newCol][obstacle])
+                        {
+                            q.push({newRow,newCol,obstacle});
+                            visited[newRow][newCol][obstacle]=true;
+                        }
+                        if(grid[newRow][newCol]==1 and obstacle>0 and !visited[newRow][newCol][obstacle-1])
+                        {
+                            q.push({newRow,newCol,obstacle-1});
+                            visited[newRow][newCol][obstacle-1]=true;
+                        }
+                    }
+                }
             }
-            ++cnt;
+            countSteps++;
         }
-
         return -1;
     }
 };
