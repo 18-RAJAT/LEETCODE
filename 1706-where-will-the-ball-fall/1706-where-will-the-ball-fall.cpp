6c89268ca14268c42f9ca20effc796a4d8e1188a
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-11-01 22:31:53 +0530

    Time: 57 ms (46.62%), Space: 13.2 MB (52.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,59 +1,31 @@
 class Solution {
 public:
-    int fUtils(vector<vector<int>>& grid, int row, int col, int rows, int cols)
-    {
-        //within the bounds, just dont put row check
-        if(row < 0 || col < 0 || col >= cols)
-        {
-            return -1;
-        }
-        
-        //row check for case when ball exits
-        if(row == rows)
-        {
-            return col;
-        }
-        
-        if(grid[row][col] == 1)
-        {
-            if(col + 1 < cols && grid[row][col+1] == 1)
-            {
-                return fUtils(grid, row + 1, col + 1, rows, cols);
-            }
-            else
-            {
-                //opposite when opposite in V
-                return -1;
-            }
-        }
-        else
-        {
-            if(col - 1 >= 0 && grid[row][col-1] == -1)
-            {
-                return fUtils(grid, row + 1, col - 1, rows, cols);
-            }
-            else
-            {
-                return -1;//stuck when opposite in V
-            }
-        }
-        
-    }
-    
     vector<int> findBall(vector<vector<int>>& grid) {
-        
-        int rows = grid.size();
-        int cols = grid[0].size();
-        
-        vector<int> res;
-        
-        for(int i=0;i<cols;i++)
+        int rows=grid.size();
+        int cols=grid[0].size();
+        vector<int>dp(cols,-1);
+        for(int c=0,i,j;c<cols;++c) 
         {
-            //throwing balls from each cols
-            int val = fUtils(grid, 0, i, rows, cols);
-            res.push_back(val);
+            for(i=0,j=c;i<rows;++i) 
+            {
+                if (grid[i][j]==1 and j<cols-1 and grid[i][j+1]==1)
+                {
+                    j++;
+                }
+                else if (grid[i][j]==-1 and j>0 and grid[i][j-1]==-1) 
+                {
+                    j--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if(i==rows) 
+            {
+                dp[c]=j;
+            }
         }
-        
-        return res;
+        return dp;
     }
 };
