946ef245c902d172cfdb547ed8c9654d60462f0d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-11-12 14:59:27 +0530

    Time: 365 ms (89.98%), Space: 117.1 MB (65.05%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,32 +1,41 @@
 class MedianFinder {
-    
-    priority_queue<int> max; //max heap for first half
-    priority_queue<int, vector<int>, greater<int>> min; //min heap for next half
+private:priority_queue<int> que1;
+        priority_queue<int, vector<int>, greater<int> > que2;
 public:
-    MedianFinder() {
-     
+
+    // Adds a number into the data structure.
+    void addNum(int num) {
+        if(que1.empty() &&que2.empty()) {que1.push(num);return;}
+        double mid=findMedian();
+        if(num<mid) {que1.push(num);
+            if(que1.size()-que2.size()>1){
+               int t=que1.top();
+               que1.pop();
+               que2.push(t);
+            }
+        }else{
+            que2.push(num);
+            if(que2.size()-que1.size()>=1){
+               int t=que2.top();
+               que2.pop();
+               que1.push(t);
+            }
+        }
+        return;
         
     }
-    
-    void addNum(int num) {
-        max.push(num);
-        min.push(max.top());
-        max.pop();
-        if(min.size() > max.size())
-        {    
-            max.push(min.top());
-            min.pop();
-        }
-    }
-    
+
+    // Returns the median of current data stream
     double findMedian() {
-        if(max.size() > min.size())
-            return max.top();
-        else
-            return (max.top()+min.top())/2.0;
+        if(que1.empty() &&que2.empty()) return 0;
+        if(que1.size()==que2.size()){
+            double m=que1.top(),n=que2.top();
+            return (m+n)/2;
+        }
+        if(que1.size()>que2.size()) return (double)que1.top();
+        else return (double)que2.top();
     }
 };
-
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
