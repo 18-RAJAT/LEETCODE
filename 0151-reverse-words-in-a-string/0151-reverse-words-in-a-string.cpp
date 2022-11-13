99057782bf7809762dfa18cf91a2ed67746e1293
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-11-13 12:03:23 +0530

    Time: 10 ms (66.39%), Space: 7.5 MB (67.61%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,59 @@
+#define MP make_pair
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define ll long long
+#define all(x) sort(x.begin(), x.end())
+#define ff(a,b) for(int i=a;i<b;i++)
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define FOREACH(n) for(auto it:n)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<vector<int>> VII;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<PII> VIII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef priority_queue<pair<int,pair<int,int>>> PQVI;
+typedef unordered_map<string,vector<string>> MPSVS;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<string>st;
+        f1(i,0,s.length())
+        {
+            string word="";
+            if(s[i]==' ')
+            {
+                continue;
+            }
+            while(s[i]!=' ' and i<s.length())
+            {
+                word+=s[i];
+                ++i;
+            }
+            st.push(word);
+        }
+        string ans="";
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+            
+            if(!st.empty())
+            {
+                ans+=" ";
+            }
+        }
+        return ans;
+    }
+};
