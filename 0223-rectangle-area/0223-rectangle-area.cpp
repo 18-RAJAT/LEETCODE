fdc270c82f8baa922f78cd1aa8cf94233705893a
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-11-17 13:45:02 +0530

    Time: 15 ms (57.30%), Space: 5.9 MB (32.00%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int ans=(ax2-ax1)*(ay2-ay1)+(bx2-bx1)*(by2-by1)-max(min(ay2,by2)-max(by1,ay1),0)*max(min(ax2,bx2)-max(bx1,ax1),0);
+        int ans=abs(ax2-ax1)*abs(ay2-ay1)+abs(bx2-bx1)*abs(by2-by1)-max(min(ay2,by2)-max(by1,ay1),0)*max(min(ax2,bx2)-max(bx1,ax1),0);
         return ans;
     }
 };
