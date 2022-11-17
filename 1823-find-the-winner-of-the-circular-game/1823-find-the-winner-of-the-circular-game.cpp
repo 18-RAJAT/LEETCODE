a29727f4d1f7a7f6144f44e96956b0a4b72496d3
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-11-18 02:26:43 +0530

    Time: 0 ms (100.00%), Space: 6 MB (65.86%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
+        
+        //using math's
+        
         int startingPos=0;
         for(int i=1;i<=n;++i)
         {
@@ -10,5 +13,26 @@ public:
             startingPos++;
         }
         return startingPos;
+        
+        //using vector
+        
+        // vector<int>ans;
+        // int m=ans.size();
+        // for(int i=1;i<=n;++i)
+        // {
+        //     ans.push_back(i);
+        // }
+        // int startingPos=0;
+        // // while(m<=1)
+        // while(m!=1)
+        // {
+        //     startingPos=(startingPos+k-1)%m;
+        //     ans.erase(ans.begin()+startingPos);
+        // }
+        // return ans[0];
+        
+        
+        //queue solution
+        
     }
 };
