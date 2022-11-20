e06ad21048f6b6fbfc4b3ebad1c3a3f9daf49dfd
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-11-20 19:25:10 +0530

    Time: 10 ms (86.60%), Space: 8 MB (93.14%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,49 +1,48 @@
 class Solution {
 public:
-    int calculate(string s) {
-        int result=0;
-        int sum=0;
-        int sign=1;
-        stack<int>st;
-        int n=s.size();
-        for(int i=0;i<n;i++)
-        {
-            if(isdigit(s[i]))
-            {
-                sum=s[i] -'0';
-                while(i+1<n && isdigit(s[i+1]))
-                {
-                    sum = sum*10 + (s[i+1] -'0');
-                    i++;
-                }
-                result+=sum*sign;
-            }
-            
-            else if(s[i] == '+')
-            {
-                sign=1;
-            }
-            else if (s[i] == '-')
-            {
-                sign=-1;
-            }
-            else if (s[i] == '(')
-            {
-                st.push(result);
-                st.push(sign);
-                result=0;
-                sign=1;
-            }
-            else if(s[i] == ')')
-            {
-                int xsign=st.top();
-                st.pop();
-                int xresult= st.top();
-                st.pop();
-                result=result*xsign+xresult;
-            }
-        }
+
+    int calculateUtil(string &s,int &i)
+    {
+        int res=0;
+        int lastOp=1;
+        int current=0;
         
-        return result;
+        int n=s.size();
+        while(i<n)
+        {
+            if(s[i]=='(')
+            {
+                current=calculateUtil(s,++i);
+            }
+            else if(s[i]==')')
+            {
+                break;
+            }
+            else if(s[i]=='+')
+            {
+                res+=current*lastOp;
+                lastOp=1;
+                current=0;
+            }
+            else if(s[i]=='-')
+            {
+                res+=current*lastOp;
+                lastOp=-1;
+                current=0;
+            }
+            else if(s[i]!=' ')
+            {
+                current=current*10+(s[i]-'0');
+            }
+            ++i;
+        }
+        int ans=res+=current*lastOp;
+        return ans;
+    }
+
+    int calculate(string s) 
+    {
+        int i=0;
+        return calculateUtil(s,i);
     }
 };
