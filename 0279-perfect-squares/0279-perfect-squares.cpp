104b474909c355406148181a46800d4e48da4c13
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-11-22 11:19:18 +0530

    Time: 398 ms (49.26%), Space: 9.1 MB (51.59%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -6,20 +6,30 @@
 class Solution {
 public:
     int numSquares(int n) {
+    
         // int dp[n+1];
         // memset(dp,INT_MAX,sizeof(dp));
+        
+        //pruning
         vector<int>dp(n+1,INT_MAX);
         
+        //base case
         dp[0]=0;
+        
+        //cache check
         cf(i,1,n)
         {
             // long long int x=sqrt(n);
             // cf(j,2,x)
             for(int j=1;j*j<=i;j++)
             {
+                //compute
                 dp[i]=min(dp[i],dp[i-j*j]+1);
+                // cout<<dp[i]<<endl;
             }
         }
+        //save and return
         return dp[n];
+        // cout<<dp[n]<<endl;
     }
 };
