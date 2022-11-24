f26eefec6ba31b152ed0636d9061c65b47ef603a
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-11-24 19:52:53 +0530

    Time: 1849 ms (10.55%), Space: 10.8 MB (23.45%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,32 +1,86 @@
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n=nums.size();
+        
+//         //level
+//         int dp[n];
+//         memset(dp,0,sizeof(dp));
+        
+//         int ct=0;
+//         dp[0]=1;
+//         int answer=1;
+//         //choice
+//         for(int i=1;i<n;++i)
+//         {
+//             for(int j=n-1;j>=0;--j)
+//             {
+//                 //check
+//                 if(nums[i]>nums[j] and dp[i]<dp[j])
+//                 {
+//                     //move
+//                     dp[i]=dp[j];
+//                 }
+//             }
+//             dp[i]++;
+//             answer=max(answer,dp[i]);
+//         }
+//         // answer=max(answer,dp[i]);
+//         //save and return
+//         return answer;
+//     }
+// };
+
+
+
+
+//recursive soln
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    int recursive(vector<int>&nums,vector<int>&dp,int idx)
+    {
         int n=nums.size();
-        
-        //level
-        int dp[n];
-        memset(dp,0,sizeof(dp));
-        
-        int ct=0;
-        dp[0]=1;
-        int answer=1;
-        //choice
-        for(int i=1;i<n;++i)
+        // int idx=0;
+        if(idx==n)
         {
-            for(int j=n-1;j>=0;--j)
-            {
-                //check
-                if(nums[i]>nums[j] and dp[i]<dp[j])
-                {
-                    //move
-                    dp[i]=dp[j];
-                }
-            }
-            dp[i]++;
-            answer=max(answer,dp[i]);
+            return 0;
         }
-        // answer=max(answer,dp[i]);
-        //save and return
-        return answer;
+        
+        if(dp[idx]>1)
+        {
+            return dp[idx];
+        }
+        
+        int ans=0;
+        for(int i=idx+1;i<n;++i)
+        {
+            if(nums[idx]<nums[i])
+            {
+                //max
+                ans=max(1+recursive(nums,dp,i),ans);
+            }
+        }
+        return dp[idx]=ans;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int mx=0;
+        int n=nums.size();
+        // int dp[n];int mx;
+        vector<int>dp(n);
+        // dp[n]=1;        
+        // int mx=0;
+        for(int i=0;i<n;++i)
+        {
+            dp[i]=1;
+        }
+        
+        for(int i=0;i<n;++i)
+        {
+            mx=max(1+recursive(nums,dp,i),mx);
+            // mx++;
+        }
+        return mx;
+        // cout<<mx<<"\n";
     }
 };
