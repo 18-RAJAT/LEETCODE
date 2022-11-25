e72e4fedffd5b7a6bf21dfc45785da401e95a17c
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-11-25 09:27:39 +0530

    Time: 2 ms (69.78%), Space: 6 MB (38.10%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,37 +1,62 @@
+
 // class Solution {
 // public:
 //     int fib(int n) {
 
 //             //USING RECURSION
-
 //             if(n==0 or n==1){return n;}
 //             int subproblem1=fib(n-1);
 //             int subproblem2=fib(n-2);
 //             return subproblem1+subproblem2;
 //     }
-// };
-
+// };  
   
-//USING DP
-// class Solution {
-// public:
-//     int fib(int n) {
-//         if(n<2){return n;}int dp[n+1];dp[0]=0,dp[1]=1;
-//         for(int i=2;i<=n;++i)
-//         {dp[i]=dp[i-1]+dp[i-2];}
-//             return dp[n];
-//     }
-// };
 
 
-
-//3rd way
+// USING DP
 class Solution {
 public:
     int fib(int n) {
         if(n<2){return n;}
-        int p1=0,p2=1;
-        while(--n){int tp=p1+p2;p1=p2;p2=tp;}
-        return p2;
+        int dp[n+1];
+        dp[0]=0,dp[1]=1;
+        for(int i=2;i<=n;++i)
+        {dp[i]=dp[i-1]+dp[i-2];}
+            return dp[n];
     }
 };
+
+
+// class Solution {
+// public:
+//     int fib(int n) {
+//         int curr=0,prev=1;
+//         if(n<=1)
+//         {
+//             return n;
+//         }
+//         for(int i=2;i<=n;i++)
+//         {
+//             int temp = curr+prev;
+//             // prev = curr;
+//             // curr = temp;
+//             // curr=temp;
+//             // prev=curr;
+//             curr=prev;
+//             // curr=temp;
+//             prev=temp;
+//         }
+//         return prev;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n<2){return n;}
+//         int p1=0,p2=1;
+//         while(--n){int tp=p1+p2;p1=p2;p2=tp;}
+//         return p2;
+//     }
+// };
