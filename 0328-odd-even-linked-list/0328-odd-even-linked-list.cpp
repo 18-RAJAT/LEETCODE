88aa699ab6d1bcfc8cb8c44da1828a13c7649c3f
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-12-06 17:47:24 +0530

    Time: 11 ms (95.59%), Space: 10.8 MB (5.12%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,24 +11,39 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head==nullptr or head->next==nullptr or head->next->next==nullptr)
-        {
-            return head;
-        }
-        ListNode*oddtail=head;
-        ListNode*oddhead=head;
-        ListNode*eventail=head->next;
-        ListNode*evenhead=head->next;
         
-        while(eventail!=nullptr and eventail->next!=nullptr)
+        //Taking the virtually head of odd
+        ListNode* oddHead=new ListNode(0);
+        ListNode* odd = oddHead;
+        
+        //Taking the virtually head of even
+        ListNode* evenHead=new ListNode(0);
+        ListNode* even=evenHead;
+        
+        ListNode* current=head;
+        
+        int counter=1;
+        
+        while(current)
         {
-            oddtail->next = eventail->next;
-            oddtail = eventail->next;
-            
-            eventail->next = oddtail->next;
-            eventail = oddtail->next;
+            if(counter%2==1)
+            {
+//odd next value is current then we will pointing next one like odd -> next same as even case
+                odd->next=current;
+                odd=odd->next;
+            }
+            else
+            {
+                //Checking even case 
+                even->next=new ListNode(current->val);
+                even = even->next;
+            }
+            counter++;
+            current=current->next;
         }
-        oddtail->next=evenhead;
-        return oddhead;
+         //Attatch all of them 
+        odd->next=evenHead->next;
+        
+        return oddHead->next;
     }
 };
