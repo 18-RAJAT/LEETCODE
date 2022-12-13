00d8bd162ddb7aaa3e43f27d6f6b551e94b5ac18
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-12-13 12:28:06 +0530

    Time: 27 ms (65.80%), Space: 9.8 MB (84.29%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -5,33 +5,56 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
         int dp[n][m];
-        for(int i=0;i<m;i++)
+        
+        for(int i=0;i<m;++i)
         {
             dp[0][i]=matrix[0][i];
         }
-        for(int i=1;i<n;i++)
+        
+        //iterate over the matrix row wise
+        for(int i=1;i<n;++i)
         {
-            for(int j=0;j<m;j++)
+            //iterate over the columns
+            for(int j=0;j<m;++j)
             {
                 if(j==0)
                 {
-                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j+1]);
+                    //find the minimum of the previous row
+                    int mini=min(dp[i-1][j],dp[i-1][j+1]);
+                    //add the current element to the minimum of the previous row
+                    dp[i][j]=matrix[i][j]+mini;
                 }
+                
                 else if(j==m-1)
                 {
-                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j-1]);
+                    //same as previous state but we have to find the minimum of the previous row
+                    int mini1=min(dp[i-1][j],dp[i-1][j-1]);
+                    dp[i][j]=matrix[i][j]+mini1;
                 }
+                
                 else
                 {
-                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],min(dp[i-1][j-1],dp[i-1][j+1]));
+                    //find the minimum of the previous row and add the current element to it
+                    int mini2=min({dp[i-1][j],dp[i-1][j-1],dp[i-1][j+1]});
+                    dp[i][j]=matrix[i][j]+mini2;
                 }
             }
         }
-        int ans=INT_MAX;
-        for(int i=0;i<m;i++)
+        
+        // for(int i=0;i<n;++i)
+        // {
+        //     for(int j=0;j<m;++j)
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<"\n"; 
+        // }
+
+        int result=INT_MAX;
+        for(int i=0;i<m;++i)
         {
-            ans=min(ans,dp[n-1][i]);
+            result=min(result,dp[n-1][i]);
         }
-        return ans;       
+        return result;
     }
 };
