d238c6e04ceae27a944706bf1c13cd42bb7a8d39
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-12-24 23:42:05 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (60.79%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,15 +1,23 @@
+const int MOD=1000000007;
 class Solution {
 public:
     int numTilings(int n) {
-        vector<int>dp(n+5,0);
-        int mod=1000000007;
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=5;
+        vector<int> ans;
+        ans.push_back(0);
+        ans.push_back(1);
+        ans.push_back(2);
+        ans.push_back(5);
+        if(n<=3)
+        {
+            return ans[n];
+        }
+        int j=1;
         for(int i=4;i<=n;i++)
         {
-            dp[i]=((2*((dp[i-1])%mod))%mod+(dp[i-3])%mod)%(mod);
+            int x=(2*(ans[i-1]%MOD)%MOD+ans[j]%MOD)%MOD;
+            ans.push_back(x);
+            j++;
         }
-        return dp[n];
+        return ans[n];
     }
 };
