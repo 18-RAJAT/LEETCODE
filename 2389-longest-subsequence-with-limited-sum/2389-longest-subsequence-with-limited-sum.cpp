af5eddbaa32cf0835fd5e87ab3c75cec0a6103e7
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-12-25 09:20:09 +0530

    Time: 19 ms (92.50%), Space: 13.8 MB (65.60%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,54 +1,17 @@
-#define MP make_pair
-#define PB push_back
-#define INF (int)1e9
-#define EPS 1e-9
-#define MOD 1000000007
-#define PI 3.1415926535897932384626433832795
-#define read(type) readInt<type>()
-const double pi=acos(-1.0);
-#define ll long long
-typedef pair<int, int> PII;
-typedef vector<int> VI;
-typedef vector<vector<int>> VII;
-typedef vector<string> VS;
-typedef vector<vector<string>> VVS;
-typedef vector<PII> VIII;
-typedef vector<VI> VVI;
-typedef map<int,int> MPII;
-typedef priority_queue<pair<int,pair<int,int>>> PQVI;
-typedef unordered_map<string,vector<string>> MPSVS;
-typedef set<int> SETI;
-typedef multiset<int> MSETI;
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int  uint64;
-#define all(x) sort(x.begin(), x.end())
-#define ff(a,b) for(int i=a;i<b;i++)
-#define f1(i,s,e) for(long long int i=s;i<e;i++)
-#define ff1(pass) for(long long int i=n-2;i>=0;--i)
-#define cf(i,s,e) for(long long int i=s;i<=e;i++)
-#define FOREACH(n) for(auto it:n)
-#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
-#define pb push_back
-#define eb emplace_back
-
-
-
 class Solution {
 public:
-    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
-        sort(nums.begin(),nums.end());
-        VI ans;
-        for(auto it:queries)
+    vector<int> answerQueries(vector<int>& num, vector<int>& q) {
+        vector<int>ans;
+        sort(num.begin(),num.end());
+        for(int i=1;i<num.size();i++)
         {
-            int sum=0,count=0;
-            f1(i,0,nums.size())
-            {
-                sum=sum+nums[i];
-                if(sum<=it){count++;}
-            }
-            ans.pb(count);
+            num[i]+=num[i-1];
+        }
+        
+        for(int i=0;i<q.size();i++)
+        {
+            int ind=upper_bound(num.begin(),num.end(),q[i])-num.begin();
+            ans.push_back(ind);
         }
         return ans;
     }
