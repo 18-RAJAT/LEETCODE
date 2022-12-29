25a38b2221e1b11c5e45faa6e11e8f62615a708b
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-12-29 19:26:32 +0530

    Time: 686 ms (82.80%), Space: 129.4 MB (68.25%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,54 +1,28 @@
 class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
-        auto tsk_cmp = [&tasks](const int i, const int j){
-            const vector<int> & a = tasks[i];
-            const vector<int> & b = tasks[j];
-            return a[1] == b[1] ? i > j : a[1] > b[1];
-        };
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        long n=tasks.size();
+        long time=0,i=0;
+        for(int i=0;i<n;i++)
+            tasks[i].push_back(i);
+            sort(tasks.begin(),tasks.end());
+            vector<int>v;
 
-        map<int, vector<int>> timer;
-        priority_queue<int, vector<int>, decltype(tsk_cmp)> q(tsk_cmp);
-        int n = tasks.size();
-        for (int i = 0; i < n; i++)
+        while(i<n or !pq.empty())
         {
-            timer[tasks[i][0]].push_back(i);
-        }
-
-        vector<int> ans;
-
-        auto t = timer.begin();
-        int now = 1;
-        while (t != timer.end())
-        {
-            while (t != timer.end() && t->first <= now)
+            if(pq.empty())
+                time=max(time,(long)tasks[i][0]);
+            while(i<n and time>=tasks[i][0])
             {
-                for (int i : t->second)
-                {
-                    q.push(i);
-                }
-                t ++;
+                pq.push(make_pair(tasks[i][1],tasks[i][2]));
+                i++;
             }
-            
-            if (q.empty())
-            {
-                now = t->first;
-                continue;
-            }
-
-            int i = q.top();
-            ans.push_back(i);
-            now += tasks[i][1];
-            q.pop();
+            pair<int,int>st=pq.top();
+            pq.pop();
+            time+=st.first;
+            v.push_back(st.second);
         }
-
-        while (!q.empty())
-        {
-            int i = q.top();
-            ans.push_back(i);
-            q.pop();
-        }
-
-        return ans;
+        return v;
     }
 };
