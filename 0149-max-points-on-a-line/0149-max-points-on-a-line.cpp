39811dc664c8e6c4c271727f55aff32d83abfb1b
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-01-08 11:59:00 +0530

    Time: 22 ms (98.39%), Space: 12.5 MB (70.67%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -2,34 +2,41 @@ class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         int n=points.size();
-        if(n<=2)return n;
-        int ans=0;
-        for(int i=0;i<n;i++)
+        if(n<2)
         {
-            int same=1;
-            int vertical=0;
-            int maxPoints=0;
-            unordered_map<double,int>mp;
-            for(int j=i+1;j<n;j++)
+            return n;
+        }
+        int ans=0;
+        for(int i=0;i<n;++i)
+        {
+            int point1=points[i][0];
+            int point2=points[i][1];
+            int same=1,vertical=0,maxPoint=0;
+            map<double,int>mp;
+            for(int j=i+1;j<n;++j)
             {
-                if(points[i][0]==points[j][0] && points[i][1]==points[j][1])
+                int pts1=points[j][0];
+                int pts2=points[j][1];
+                
+                if(point1==pts1 and point2==pts2)
                 {
                     same++;
                 }
-                else if(points[i][0]==points[j][0])
+                else if(point1==pts1)
                 {
                     vertical++;
                 }
                 else
                 {
-                    double slope=(double)(points[i][1]-points[j][1])/(points[i][0]-points[j][0]);
+                    double slope=(double)(pts2-point2)/(pts1-point1);
                     mp[slope]++;
-                    maxPoints=max(maxPoints,mp[slope]);
+                    maxPoint=max(maxPoint,mp[slope]);
                 }
             }
-            maxPoints=max(maxPoints,vertical);
-            ans=max(ans,maxPoints+same);
+            maxPoint=max(maxPoint,vertical);
+            int isSame=maxPoint+same;
+            ans=max(ans,isSame);
         }
-        return ans;      
+        return ans;     
     }
 };
