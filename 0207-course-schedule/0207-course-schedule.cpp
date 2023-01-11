0e67fbcfe2737fc69cdb9840872d66947bae8cd7
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-01-11 21:21:11 +0530

    Time: 25 ms (79.83%), Space: 13.3 MB (82.02%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
+        queue<int>q;
+        
+        for(vector<int>&edge:prerequisites)
+        {
+            int u=edge[1];
+            int v=edge[0];
+            
+            adj[u].push_back(v);
+            indegree[v]++;
+            
+            // cout<<"Degree: "<<indegree[v]<<"\n";
+        }
+        for(int i=0;i<numCourses;++i)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        //topo sort
+        while(not q.empty())
+        {
+            int tp=q.front();
+            q.pop();
+            
+            for(auto it:adj[tp])
+            {
+                indegree[it]--;
+                
+                if(indegree[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        
+        //if indegree of all nodes is 0,then all courses are finished else we will not finish
+        for(int i=0;i<numCourses;++i)
+        {
+            if(indegree[i])
+            {
+                 return false;
+            }
+        }
+        return true;
+    }
+};
