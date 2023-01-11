f5541e7b911fb7d84ac0225abee480ec1aa8d4d4
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-01-11 22:02:51 +0530

    Time: 22 ms (88.25%), Space: 13.2 MB (90.98%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -4,15 +4,15 @@ public:
         vector<vector<int>>adj(numCourses);
         vector<int>indegree(numCourses,0);
         queue<int>q;
-        
+
         for(vector<int>&edge:prerequisites)
         {
             int u=edge[1];
             int v=edge[0];
-            
+
             adj[u].push_back(v);
             indegree[v]++;
-            
+
             // cout<<"Degree: "<<indegree[v]<<"\n";
         }
         for(int i=0;i<numCourses;++i)
@@ -22,16 +22,17 @@ public:
                 q.push(i);
             }
         }
+        
         //topo sort
         while(not q.empty())
         {
             int tp=q.front();
             q.pop();
-            
+
             for(auto it:adj[tp])
             {
                 indegree[it]--;
-                
+
                 if(indegree[it]==0)
                 {
                     q.push(it);
