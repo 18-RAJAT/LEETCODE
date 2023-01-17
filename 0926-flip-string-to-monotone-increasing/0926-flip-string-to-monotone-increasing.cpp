b51a92e796e70f44596c9f4ad93ab1bb6ac93e9b
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-01-17 20:22:05 +0530

    Time: 22 ms (89.87%), Space: 11.1 MB (82.03%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
+        // int n=s.length();
+        // int dp[n+1][2];
+        // dp[0][0]=0;
+        // dp[1][0]=0;
+        // for(int i=1;i<=n;++i)
+        // {
+        //     if(s[i-1]=='0')
+        //     {
+        //         dp[i][0]=dp[i-1][0];
+        //         dp[i][1]=1+min(dp[i-1][0],dp[i-1][1]);
+        //     }
+        //     else
+        //     {
+        //         dp[i][0]=1+dp[i-1][0];
+        //         dp[i][1]=min(dp[i-1][0],dp[i-1][1]);
+        //     }
+        // }
+        // return min(dp[n][0],dp[n][1]);
+        
+        
+        //Another Approach
+        int flips=0;
+        int ct=0;
         int n=s.length();
-        int dp[n+1][2];
-        dp[0][0]=0;
-        dp[1][0]=0;
-        for(int i=1;i<=n;++i)
+        for(int i=0;i<n;++i)
         {
-            if(s[i-1]=='0')
+            if(s[i]=='1')
             {
-                dp[i][0]=dp[i-1][0];
-                dp[i][1]=1+min(dp[i-1][0],dp[i-1][1]);
+                ct++;
             }
             else
             {
-                dp[i][0]=1+dp[i-1][0];
-                dp[i][1]=min(dp[i-1][0],dp[i-1][1]);
+                flips++;
             }
+            flips=min(flips,ct);
         }
-        return min(dp[n][0],dp[n][1]);
+        return flips;
     }
 };
