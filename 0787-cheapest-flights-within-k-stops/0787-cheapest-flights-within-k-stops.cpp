8a9765586e1883aed4460e17d5e3923e51122833
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-01-26 10:45:56 +0530

    Time: 38 ms (51.23%), Space: 12.5 MB (98.74%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,25 +2,24 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        
         auto bellmanFord=[&]()->int
         {
             vector<int>dist(n,INT_MAX);
-            dist[src]=0;
+            dist[src]=0;//initial point
             for(int i=0;i<=k;i++)
             {
-                vector<int>temp=dist;
-                for(auto &x:flights)
+                vector<int>dist1=dist;
+                for(auto &edge:flights)
                 {
-                    int u=x[0];
-                    int v=x[1];
-                    int w=x[2];
+                    int u=edge[0];
+                    int v=edge[1];
+                    int w=edge[2];
                     if(dist[u]!=INT_MAX)
                     {
-                        temp[v]=min(temp[v],dist[u]+w);
+                        dist1[v]=min(dist1[v],dist[u]+w);
                     }
                 }
-                dist=temp;
+                dist=dist1;
             }
             if(dist[dst]==INT_MAX)
             {
