8a3c79d42db4b1bc6840d7522e5f6d3c0c662bc9
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-01-30 21:35:42 +0530

    Time: 6 ms (50.43%), Space: 13.6 MB (58.25%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    void generate(vector<string>&ans,int open,int close,string s)
+    void par(vector<string>&brac,int open,int close,string s)
     {
-            if(open==0 and close==0)
-            {
-                ans.push_back(s);
-                return;
-            }
-            if(open>0)
-            {
-                generate(ans,open-1,close+1,s+"(");
-            }
-            if(close>0)
-            {
-                generate(ans,open,close-1,s+")");
-            }
+        if(open==0 and close==0)
+        {
+            brac.push_back(s);
+            return;
+        }
+        if(open>0)
+        {
+            par(brac,open-1,close+1,s+"(");
+        }
+        if(close>0)
+        {
+            par(brac,open,close-1,s+")");
+        }
     }
     vector<string> generateParenthesis(int n) {
-        vector<string>ans;
-        generate(ans,n,0,"");
-        return ans;
+        vector<string>brac;
+        par(brac,n,0,"");
+        return brac;
     }
 };
