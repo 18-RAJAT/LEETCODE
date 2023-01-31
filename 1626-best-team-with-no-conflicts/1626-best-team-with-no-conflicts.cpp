c18b34816ba1b9d1946b86a645aad4b5820c1755
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-01-31 20:02:01 +0530

    Time: 277 ms (52.92%), Space: 23 MB (39.93%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -10,31 +10,60 @@ public:
         many cases will be created and then to return maximum.
     */
     
-    //Time complexity O(n^2)
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         int n=scores.size();
-        vector<pair<int,int>>best;
+        
+        //1st
+        //Time complexity O(n^2)
+        
+        // vector<pair<int,int>>best;
+        // for(int i=0;i<n;++i)
+        // {
+        //     best.push_back(make_pair(ages[i],scores[i]));
+        // }
+        // sort(best.begin(),best.end());
+        // vector<int>dp(n,0);
+        // dp[0]=best[0].second;
+        // for(int i=1;i<n;++i)
+        // {
+        //     //if we dont take anyone before this person then we can take this person only
+        //     dp[i]=best[i].second;
+        //     for(int j=0;j<i;++j)
+        //     {
+        //         //if we take this person then we can take anyone before this person
+        //         if(best[i].second>=best[j].second)
+        //         {
+        //             //we will take the maximum of the two cases current+previois
+        //             dp[i]=max(dp[i],dp[j]+best[i].second);
+        //         }
+        //     }
+        // }
+        // return *max_element(dp.begin(),dp.end());
+        
+        
+        //2nd
+        int ans=0;
+        int dp[n];
+        memset(dp,0,sizeof(dp));
+        vector<vector<int>>team;
         for(int i=0;i<n;++i)
         {
-            best.push_back(make_pair(ages[i],scores[i]));
+            team.push_back({ages[i],scores[i]});
         }
-        sort(best.begin(),best.end());
-        vector<int>dp(n,0);
-        dp[0]=best[0].second;
-        for(int i=1;i<n;++i)
+        sort(team.begin(),team.end());
+        for(int i=0;i<n;++i)
         {
-            //if we dont take anyone before this person then we can take this person only
-            dp[i]=best[i].second;
+            //score
+            dp[i]=team[i][1];
             for(int j=0;j<i;++j)
             {
-                //if we take this person then we can take anyone before this person
-                if(best[i].second>=best[j].second)
+                if(team[j][1]<=team[i][1])
                 {
-                    //we will take the maximum of the two cases current+previois
-                    dp[i]=max(dp[i],dp[j]+best[i].second);
+                    dp[i]=max(dp[i],dp[j]+team[i][1]);
                 }
             }
+            ans=max(ans,dp[i]);
         }
-        return *max_element(dp.begin(),dp.end());
+        return ans;
     }
 };
