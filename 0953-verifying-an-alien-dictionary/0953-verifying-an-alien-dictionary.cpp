7a404f9288937bab041502f4da1f1ac7504b22af
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-03 01:23:31 +0530

    Time: 4 ms (77.79%), Space: 9.7 MB (25.62%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,45 +1,69 @@
+// class Solution {
+// public:
+//     bool isSorted(string& s1,string& s2,string& order)
+//     {
+//         int n=s1.length();
+//         int m=s2.length();
+        
+//         int mn=min(n,m);
+//         for(int i=0;i<mn;++i)
+//         {
+//             int idx1=order.find(s1[i]);
+//             int idx2=order.find(s2[i]);
+            
+//             if(idx1==idx2)
+//             {
+//                 continue;
+//             }
+//             if(idx1<idx2)
+//             {
+//                 return true;
+//             }
+//             if(idx1>idx2)
+//             {
+//                 return false;
+//             }
+//         }
+//         return n<=m;
+//     }
+//     bool isAlienSorted(vector<string>& words, string order) {
+//         map<char,int>chk;
+//         for(int i=0;i<26;++i)
+//         {
+//             chk[order[i]]=i;
+//         }
+//         int n=words.size();
+//         for(int i=1;i<n;++i)
+//         {
+//             if(not isSorted(words[i-1],words[i],order))
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+
+
+
+//2nd Approach
 class Solution {
 public:
-    bool isSorted(string& s1,string& s2,string& order)
-    {
-        int n=s1.length();
-        int m=s2.length();
-        
-        int mn=min(n,m);
-        for(int i=0;i<mn;++i)
-        {
-            int idx1=order.find(s1[i]);
-            int idx2=order.find(s2[i]);
-            
-            if(idx1==idx2)
-            {
-                continue;
-            }
-            if(idx1<idx2)
-            {
-                return true;
-            }
-            if(idx1>idx2)
-            {
-                return false;
-            }
-        }
-        return n<=m;
-    }
     bool isAlienSorted(vector<string>& words, string order) {
-        map<char,int>chk;
+        map<char,int>mp;
         for(int i=0;i<26;++i)
         {
-            chk[order[i]]=i;
+            mp[order[i]-'a']=i;
         }
-        int n=words.size();
-        for(int i=1;i<n;++i)
+        for(auto& it:words)
         {
-            if(not isSorted(words[i-1],words[i],order))
+            for(auto& ch:it)
             {
-                return false;
+                ch=mp[ch-'a'];
             }
         }
-        return true;
+        return is_sorted(words.begin(),words.end());
     }
 };
