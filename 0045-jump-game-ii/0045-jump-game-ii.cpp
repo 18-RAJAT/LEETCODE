9e84125b47d221c2928ac9de21ee6132dd1fda73
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-08 10:31:18 +0530

    Time: 19 ms (69.69%), Space: 16.4 MB (97.49%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        //1st way
         int n=nums.size();
-        int dp[n];
-        memset(dp,0,sizeof(dp));
-        dp[0]=0;
-        for(int i=1;i<n;i++)
+        // int dp[n];
+        // memset(dp,0,sizeof(dp));
+        // dp[0]=0;
+        // for(int i=1;i<n;i++)
+        // {
+        //     dp[i]=INT_MAX;
+        //     for(int j=0;j<i;j++)
+        //     {
+        //         if(i<=j+nums[j])
+        //         {
+        //             dp[i]=min(dp[i],dp[j]+1);
+        //         }
+        //     }
+        // }
+        // return dp[n-1];
+        
+        //2nd way without dp
+        int jmp=0,end=0,atMx=0;
+        for(int i=0;i<n-1;++i)
         {
-            dp[i]=INT_MAX;
-            for(int j=0;j<i;j++)
+            atMx=max(atMx,i+nums[i]);
+            if(i==end)
             {
-                if(i<=j+nums[j])
-                {
-                    dp[i]=min(dp[i],dp[j]+1);
-                }
+                end=atMx;jmp++;
             }
         }
-        return dp[n-1];
+        return jmp;
     }
 };
