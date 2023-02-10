2390e9ccaf7ee2704529bd2de650aa652e3df41d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-10 19:34:25 +0530

    Time: 123 ms (29.23%), Space: 20 MB (61.81%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,53 +1,68 @@
 class Solution {
 public:
-    struct node{
-        int x,y;
-        int dist;
-    };
     int maxDistance(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        queue<node>q;
-        for(int i=0;i<n;i++)
+        auto cmp=[](pair<int,int>a,pair<int,int>b)
         {
-            for(int j=0;j<m;j++)
+            return a.first>b.first;
+        };
+        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(cmp)>pq(cmp);
+        int n=grid.size();
+        // int m=grid[0].size();
+        for(int i=0;i<n;++i)
+        {
+            for(int j=0;j<n;++j)
             {
+                //chk with 1,then 0 and agian this process<<<<<----
                 if(grid[i][j]==1)
                 {
-                    q.push({i,j,0});
+                    pq.push(make_pair(0,i*n+j));
                 }
             }
         }
-        int ans=0;
-        while(!q.empty())
+        //sz=NxN grid or chk with the sz
+        //base case
+        if(pq.size()==n*n or pq.size()==0)
         {
-            node temp=q.front();
-            q.pop();
-            ans=max(ans,temp.dist);
-            int x=temp.x;
-            int y=temp.y;
-            if(x+1<n and grid[x+1][y]==0)
+            return -1;
+        }
+        int ans=0;
+        //assumption
+        while(not pq.empty())
+        {
+            pair<int,int>pr=pq.top();pq.pop();
+            int sc1=pr.second/n;//1
+            int sc2=pr.second%n;//2
+            
+            ans=max(ans,pr.first);
+            //dist1->assumption
+            if(sc1-1>=0 and grid[sc1-1][sc2]==0)
             {
-                grid[x+1][y]=1;
-                q.push({x+1,y,temp.dist+1});
+                grid[sc1-1][sc2]=1;
+                pq.push(make_pair(pr.first+1,(sc1-1)*n+sc2));
+                // cout<<sc1<<"->"<<sc2<<"\n";
             }
-            if(x-1>=0 and grid[x-1][y]==0)
+            //dist2->assumption
+            if(sc1+1<n and grid[sc1+1][sc2]==0)
             {
-                grid[x-1][y]=1;
-                q.push({x-1,y,temp.dist+1});
+                grid[sc1+1][sc2]=1;
+                pq.push(make_pair(pr.first+1,(sc1+1)*n+sc2));
+                // cout<<sc1<<"->"<<sc2<<"\n";
             }
-            if(y+1<m and grid[x][y+1]==0)
+            //dist3->assumption
+            if((sc2-1)>=0 and grid[sc1][sc2-1]==0)
             {
-                grid[x][y+1]=1;
-                q.push({x,y+1,temp.dist+1});
+                grid[sc1][sc2-1]=1;
+                pq.push(make_pair(pr.first+1,sc1*n+sc2-1));
+                // cout<<sc1<<"->"<<sc2<<"\n";
             }
-            if(y-1>=0 and grid[x][y-1]==0)
+            //dist4->assumption
+            if(sc2+1<n and grid[sc1][sc2+1]==0)
             {
-                grid[x][y-1]=1;
-                q.push({x,y-1,temp.dist+1});
+                grid[sc1][sc2+1]=1;
+                pq.push(make_pair(pr.first+1,sc1*n+sc2+1));
+                // cout<<sc1<<"->"<<sc2<<"\n";
             }
         }
-        if(ans==0)return -1;
-        return ans;  
+        return ans;
     }
 };
