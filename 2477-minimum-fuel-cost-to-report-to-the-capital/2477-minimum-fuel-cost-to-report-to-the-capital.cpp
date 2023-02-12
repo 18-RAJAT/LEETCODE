040c5f10a8e8251ba87a1d7b76f611e0121a5821
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-12 12:29:33 +0530

    Time: 623 ms (71.89%), Space: 196.7 MB (59.18%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,29 +1,49 @@
+#define ll long long
+#define sza(x) ((int)x.size())
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define MP make_pair
+#define PB push_back
+#define PF push_front
+
 class Solution {
 public:
-long long result=0;
-    int dfs(int node,int parent,unordered_map<int,vector<int>>&adj,int seats)
-    {
-      int pas=0;
-      for(auto &x:adj[node])
-      {
-          if(x!=parent)
-          {
-            int p=dfs(x,node,adj,seats);
-            pas+=p;
-            result+=ceil(p*1.0/seats);
-          }
-      }
-        return 1+pas;
-    }
-
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-        unordered_map<int,vector<int>>adj;
-        for(auto &x:roads)
+        ll ans=0;
+        function<int(vector<vector<int>>& g,int k,int node,vector<int>&vis)>dfs=[&](vector<vector<int>>& g,int k,int node,vector<int>&vis)
         {
-            adj[x[0]].push_back(x[1]);
-            adj[x[1]].push_back(x[0]);
+            vis[node]=1;
+            ll num=1;
+            f1(i,0,sza(g[node]))
+            {  
+                if(vis[g[node][i]]==0)
+                {
+                    vis[g[node][i]]=1;
+                    num+=dfs(g,k,g[node][i],vis);
+
+                }
+            }
+            ans+=num/k;
+            if(num%k!=0)
+            {
+                ans++;
+            }
+            return num;
+        };
+
+        int sz=sza(roads);
+        vector<vector<int>>adj(sz+1);
+        f1(i,0,sz)
+        {
+            adj[roads[i][0]].PB(roads[i][1]);
+            adj[roads[i][1]].PB(roads[i][0]);
         }
-        dfs(0,-1,adj,seats);
-        return result;
+
+        vector<int>visited(sz+1,0);
+        visited[0]=1;//1
+        f1(i,0,sza(adj[0]))
+        {
+            dfs(adj,seats,adj[0][i],visited);
+        }
+        return ans;
     }
 };
