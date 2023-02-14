7bfa634d2d161cdbe372ba0c15f6a63fede21d08
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-14 09:16:51 +0530

    Time: 4 ms (50.26%), Space: 6.9 MB (31.98%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        function<char(char,char,char&)>add=[](char x,char y,char& carryBt)->char
+        {
+            int X=x-'0';
+            int Y=y-'0';
+            int C=carryBt-'0';
+            int sum=X+Y+C;
+            carryBt=sum/2+'0';
+            return sum%2+'0';
+        };
+        // a,b same length
+        if(a.size()<b.size())
+        {
+            a=string(b.size()-a.size(),'0')+a;
+        }
+        else
+        {
+            b=string(a.size()-b.size(),'0')+b;
+        }
+        //add
+        string ans;
+        char carry='0';
+        for(int i=a.size()-1;i>=0;--i)
+        {
+            ans=add(a[i],b[i],carry)+ans;
+        }
+        if(carry=='1')
+        {
+            ans=carry+ans;
+        }
+        return ans;
+    }
+};
