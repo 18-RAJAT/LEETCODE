64bb45381c63cdf9c4f780cf88585bacf9de05a2
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-14 12:24:16 +0530

    Time: 26 ms (12.47%), Space: 29.6 MB (5.46%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,20 +1,45 @@
+namespace
+{
+    pair<int,string>Get(int n)
+    {
+        vector<pair<int,string>>dataSet=
+        {
+            {1000,"M"},
+            {900,"CM"},
+            {500,"D"},
+            {400,"CD"},
+            {100,"C"},
+            {90,"XC"},
+            {50,"L"},
+            {40,"XL"},
+            {10,"X"},
+            {9,"IX"},
+            {5,"V"},
+            {4,"IV"},
+            {1,"I"},
+        };
+        for(pair<int,string>it:dataSet)
+        {
+            int f=it.first;
+            if(n-f>=0)
+            {
+                return{n-f,it.second};
+            }
+        }
+        assert(false);
+        return{0,""};
+    }
+}
 class Solution {
 public:
-       
-        const int value[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        const string roman[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        
-        string intToRoman(int num) 
-      {
-          
-          string answer = "";
-          for(int i=0;num;++i)
-          {
-              while(num >= value[i])
-              {
-                  answer+=roman[i],num-=value[i];
-              }
-          }              
-          return answer;
+    string intToRoman(int num) {
+        string ans;
+        while(num!=0)
+        {
+            pair<int,string>p=Get(num);
+            num=p.first;
+            ans.append(p.second);
+        }
+        return ans;
     }
 };
