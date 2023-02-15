0e46d5a91d386d4ec6bc1a2ca1eaefbee60e0ce2
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-15 07:08:15 +0530

    Time: 28 ms (79.50%), Space: 27.3 MB (66.17%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    vector<int> addToArrayForm(vector<int>& num, int k)
-	{
-		function<vector<int>(vector<int>, int)> add = [&](vector<int> num, int k)->vector<int> 
-    {
-			int n = num.size();
-			//this loop is for adding the k to the last digit of the number
-			for (int i = n - 1; i >= 0; --i) {
-				//adding the k to the last digit of the number
-				num[i] += k;
-				//if the number is greater than 10 then we will add the carry to the next digit
-				k = num[i] / 10;
-				//if the number is greater than 10 then we will take the remainder of the number
-				num[i] %= 10;
-			}
-			while (k > 0) {
-				//if the number is greater than 10 then we will add the carry to the next digit
-				num.insert(num.begin(), k % 10);
-				//if the number is greater than 10 then we will take the remainder of the number
-				k /= 10;
-			}
-			//returning the number
-			return num;
-		};
-		//calling the function
-		return add(num, k);
-	}
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        function<vector<int>(vector<int>,int)>add=[&](vector<int>num,int k)->vector<int>
+        {
+            int n=num.size();
+            for(int i=n-1;i>=0;--i)//This loop is for adding the k to the last digit of the number
+            {
+                num[i]+=k;
+                k=num[i]/10;
+                num[i]%=10;
+            }
+            while(k>0)
+            {
+                num.insert(num.begin(),k%10);//If the number is greater than 10 then we will add the carry to the next digit
+                k/=10;
+            }
+            return num;
+        };
+        return add(num,k);
+    }
 };
