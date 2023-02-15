f592f97d265291a8e0dcf356fd8e38f303ff97b9
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-15 18:47:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (98.06%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,37 +1,14 @@
-#define f1(i,s,e) for(int i=s;i<e;i++)
-#define ff1(i,s,e) for(int i=s;i>=e;--i)
-#define For1(i,n) (int i=0;i<n+1;++i)
-#define cf(i,s,e) for(int i=s;i<=e;i++)
-
 class Solution {
 public:
     int twoEggDrop(int n) {
-    //     int dp[n+1];
-    //     dp[0]=0;
-    //     dp[1]=1;
-    //     dp[2]=2;
-    //     for(int i=3;i<=n;i++)
-    //     {
-    //         dp[i]=INT_MAX;
-    //         for(int j=1;j<i;j++)
-    //         {
-    //             int temp=1+max(dp[j-1],dp[i-j]);
-    //             dp[i]=min(dp[i],temp);
-    //         }
-    //     }
-    //     return dp[n];     
-        // int dp[n];
-        vector<int>dp{0};
-        // memset(dp,0,sizeof(dp));
-        cf(i,1,n)
+        int cnt=0;
+        int i=1;
+        int sum=0;
+        while(sum<n)
         {
-            int mn=INT_MAX;
-            cf(j,1,i)
-            {
-                mn=min(mn,1+max(j-1,dp[i-j]));
-            }
-            dp.push_back(mn);
+            sum+=i;
+            ++i;++cnt;
         }
-        return dp.back();
+        return cnt;
     }
 };
