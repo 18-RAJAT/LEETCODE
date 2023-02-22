50c2a199b0b67232c3a4da50bb5cf29bb47bf515
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-22 10:39:22 +0530

    Time: 464 ms (28.40%), Space: 148.7 MB (32.26%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,25 +1,56 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int>dq;
+        // monotonic-queue
+        
+        // deque<int>dq;
+        // vector<int>ans;
+        // int sz=size(nums);
+        // for(int i=0;i<sz;++i)
+        // {
+        //     while(not dq.empty() and dq.front()<=i-k)
+        //     {
+        //         dq.pop_front();
+        //     }
+        //     while(not dq.empty() and nums[dq.back()]<=nums[i])
+        //     {
+        //         dq.pop_back();
+        //     }
+        //     dq.push_back(i);
+        //     if(i>=k-1)
+        //     {
+        //         ans.push_back(nums[dq.front()]);
+        //     }
+        // }
+        // return ans;
+        
+        
+        
+        //Heap(Priority-queue)
+        auto cmp=[&](pair<int,int>a,pair<int,int>b)->int
+        {
+            return a.first<b.first;
+        };
         vector<int>ans;
-        int sz=size(nums);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(cmp)>pq(cmp);
+        int sz=nums.size();
         for(int i=0;i<sz;++i)
         {
-            while(not dq.empty() and dq.front()<=i-k)
+            if(i<k)
             {
-                dq.pop_front();
+                pq.emplace(nums[i],i);
             }
-            while(not dq.empty() and nums[dq.back()]<=nums[i])
+            else
             {
-                dq.pop_back();
-            }
-            dq.push_back(i);
-            if(i>=k-1)
-            {
-                ans.push_back(nums[dq.front()]);
+                ans.push_back(pq.top().first);
+                while(not pq.empty() and pq.top().second<=i-k)
+                {
+                    pq.pop();
+                }
+                pq.emplace(nums[i],i);
             }
         }
+        ans.push_back(pq.top().first);
         return ans;
     }
 };
