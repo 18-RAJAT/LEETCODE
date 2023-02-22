302cb8a97775875954f0964a1f1af98ef9c3de27
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-22 07:13:08 +0530

    Time: 84 ms (23.74%), Space: 26.2 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -14,7 +14,6 @@ public:
             }
             return day<=days;
         };
-        
         int l=*max_element(weights.begin(),weights.end());
         int r=accumulate(weights.begin(),weights.end(),0);
         
