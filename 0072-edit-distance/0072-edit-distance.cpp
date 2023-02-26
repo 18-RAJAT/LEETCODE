7a778b58f624a66eb7fcaf2bf8ecbb67d8834cc2
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-02-26 10:46:30 +0530

    Time: 8 ms (87.54%), Space: 7.3 MB (85.47%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -7,7 +7,8 @@ public:
         };
         int m=word1.size();
         int n=word2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        int dp[m+1][n+1];
+        memset(dp,0,sizeof(dp));
         for(int i=1;i<=m;++i)
         {
             dp[i][0]=i;
