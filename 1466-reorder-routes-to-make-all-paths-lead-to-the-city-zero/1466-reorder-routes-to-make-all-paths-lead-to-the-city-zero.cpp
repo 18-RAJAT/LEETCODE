c425a8e663863d82b3bd2d3c68a39ccb990077a3
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-03-24 09:36:31 +0530

    Time: 423 ms (58.83%), Space: 116.4 MB (46.74%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,70 +1,38 @@
-#define MP make_pair
-#define PB push_back
-#define INF (int)1e9
-#define EPS 1e-9
-#define MOD 1000000007
-#define PI 3.1415926535897932384626433832795
-#define read(type) readInt<type>()
-const double pi=acos(-1.0);
-#define ll long long
-typedef pair<int, int> PII;
-typedef vector<int> VI;
-typedef vector<bool> VB;
-typedef vector<vector<int>> VII;
-typedef vector<string> VS;
-typedef vector<vector<string>> VVS;
-typedef vector<PII> VIII;
-typedef vector<VI> VVI;
-typedef map<int,int> MPII;
-typedef priority_queue<pair<int,pair<int,int>>> PQVI;
-typedef unordered_map<string,vector<string>> MPSVS;
-typedef set<int> SETI;
-typedef multiset<int> MSETI;
-typedef queue<int> QUE;
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int  uint64;
-#define all(x) sort(x.begin(), x.end())
-#define ff(a,b) for(int i=a;i<b;i++)
-#define f1(i,s,e) for(long long int i=s;i<e;i++)
-#define ff1(pass) for(long long int i=n-2;i>=0;--i)
-#define cf(i,s,e) for(long long int i=s;i<=e;i++)
-#define FOREACH(n) for(auto it:n)
-#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
-#define pb push_back
-#define eb emplace_back
-
-
 class Solution {
 public:
-    int minReorder(int n, vector<vector<int>>& connections) {
-        QUE q;
-        VB visited(n,false);
+    int minReorder(int n, vector<vector<int>>& con) {
+        vector<vector<int>> adj(n),adj1(n);
+        vector<int>vis(n,0);
+        vis[0]=1;
+        for(auto a:con)
+        {
+            adj[a[0]].push_back(a[1]);
+            adj1[a[1]].push_back(a[0]);
+        }
+        int ans=0;
+        queue<int>q;
         q.push(0);
-        VII a(n),b(n);
-        for(auto it:connections)
+        while(not q.empty())
         {
-            a[it[0]].pb(it[1]);
-            b[it[1]].pb(it[0]);
-        }
-        int result=0;
-        visited[0]=true;
-        while(!q.empty())
-        {
-            int store=q.front();q.pop();
-            for(auto it:a[store])
+            int a=q.front();
+            vis[a]=1;
+            q.pop();
+            for(auto it:adj[a])
             {
-                if(!visited[it])
-                {q.push(it);result++;visited[it]=true;}
+                if(not vis[it])
+                {
+                    ans++;
+                    q.push(it);
+                }
             }
-            
-            for(auto it:b[store])
+            for(int it:adj1[a])
             {
-                if(!visited[it])
-                {q.push(it);visited[it]=true;}
+                if(not vis[it])
+                {
+                    q.push(it);
+                }
             }
         }
-        return result;
+        return ans;
     }
 };
