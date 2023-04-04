109c8690f728babe9132ed6067f25b900f1909ae
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-04-04 20:18:00 +0530

    Time: 351 ms (92.93%), Space: 7.9 MB (85.83%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,78 +1,67 @@
 class Solution {
-    vector<int>xDirection={0,-1,0,1};
-    vector<int>yDirection={1,0,-1,0};
 public:
-    bool dfs(vector<vector<char>>&board,string& word,int i,int j,int n,int m,int idx)
-    {
-        int x=word.length();
-        if(idx>=x)
-        {
-            return true;
-        }
-        if(i<0 or j<0 or i>=n or j>=m or board[i][j]!=word[idx])//board[i][j]=='#')//board[i][j]!=word[idx])
-        {
-            return false;
-        }
-        
-        //mark the current visited
-        board[i][j]='#';
-        
-        // if(idx==n)
-        // {
-        //     return true;
-        // }
-        
-        // if(board[i][j]!=word[idx])
-        // {
-        //     return false;
-        // }
-        
-        for(int k=0;k<4;++k)
-        {
-            if(dfs(board,word,xDirection[k]+i,yDirection[k]+j,n,m,idx+1))
-            {
-                return true;
-            }
-            // else if(board[k][k]!=word[idx])
-            // {
-            //     return false;
-            // }
-        }
-        // board[i][j]=idx;
-        // {
-        //     return false;
-        // }
-        // if(board[i][j]===0)
-        // {
-        //     return false;
-        // }
-        board[i][j]=word[idx];
-        {
-            return false;
-        }
-    }
-    
     bool exist(vector<vector<char>>& board, string word) {
-        int n=board.size();
-        int m=board[0].size();
-        // int idx=0;
-        
-        for(int i=0;i<n;++i)
-        {
-            // idx++;
-            for(int j=0;j<m;++j)
-            {
-                // idx++;
-                if(board[i][j]==word[0])
-                {
-                    // cout<<idx<<endl;
-                    if(dfs(board,word,i,j,n,m,0))
-                    {
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0]) {
+                    vis[i][j] = true;
+                    if (solve(board, word, i, j, 1, vis)) {
                         return true;
                     }
+                    vis[i][j] = false;
                 }
             }
         }
+        return false;
+    }
+
+    bool solve(vector<vector<char>> &board, string &word, int row, int col, int st, vector<vector<bool>> &vis) {
+        if (st == word.size()) {
+            return true;
+        }
+        
+        int m = board.size();
+        int n = board[0].size();
+        
+        // DOWN
+        if (row + 1 < m && board[row+1][col] == word[st] && !vis[row+1][col]) {
+            vis[row+1][col] = true;
+            if (solve(board, word, row+1, col, st+1, vis)) {
+                return true;
+            }
+            vis[row+1][col] = false;
+        }
+        
+        // UP
+        if (row - 1 >= 0 && board[row-1][col] == word[st] && !vis[row-1][col]) {
+            vis[row-1][col] = true;
+            if (solve(board, word, row-1, col, st+1, vis)) {
+                return true;
+            }
+            vis[row-1][col] = false;
+        }
+        
+        // LEFT
+        if (col - 1 >= 0 && board[row][col-1] == word[st] && !vis[row][col-1]) {
+            vis[row][col-1] = true;
+            if (solve(board, word, row, col-1, st+1, vis)) {
+                return true;
+            }
+            vis[row][col-1] = false;
+        }
+        
+        // RIGHT
+        if (col + 1 < n && board[row][col+1] == word[st] && !vis[row][col+1]) {
+            vis[row][col+1] = true;
+            if (solve(board, word, row, col+1, st+1, vis)) {
+                return true;
+            }
+            vis[row][col+1] = false;
+        }
+        
         return false;
     }
 };
