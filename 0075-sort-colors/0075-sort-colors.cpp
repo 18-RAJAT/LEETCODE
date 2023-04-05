e28e493e3c517bc708973936ff586e8f8c72a679
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-04-05 08:46:26 +0530

    Time: 3 ms (53.57%), Space: 8.1 MB (92.66%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int cnt[3];
+        memset(cnt,0,sizeof(cnt));
+        
+        for(int i=0;i<n;i++)
+        {
+            //counting
+            cnt[nums[i]]++;
+        }
+        int idx=0;
+        for(int i=0;i<3;i++)
+        {
+            //placing
+            while(cnt[i]--)
+            {
+                nums[idx]=i;idx++;
+            }
+        }
+    }
+};
