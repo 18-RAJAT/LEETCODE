7e754bb1b7982b9c666bd8a301bccd79a62d02fa
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-05-30 07:04:28 +0530

    Time: 94 ms (73.22%), Space: 84.5 MB (22.69%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,31 +1,20 @@
-#define INF (int)1e9
-typedef pair<int, int> PII;
-typedef vector<int> VI;
-typedef vector<bool> VB;
-typedef vector<vector<int>> VII;
-typedef vector<string> VS;
-typedef vector<vector<string>> VVS;
-typedef vector<PII> VIII;
-typedef vector<VI> VVI;
-
 class MyHashSet {
-    VB HASH;
 public:
+    vector<bool>hash;
     MyHashSet() {
-        HASH=VB(INF,false);
+        hash=vector<bool>(10000005,0);
     }
     
     void add(int key) {
-        // VB ert;
-        HASH[key]=true;
+        hash[key]=true;
     }
     
     void remove(int key) {
-        HASH[key]=false;
+        hash[key]=false;
     }
     
     bool contains(int key) {
-        return HASH[key];//=false;
+        return hash[key]==true;
     }
 };
 
