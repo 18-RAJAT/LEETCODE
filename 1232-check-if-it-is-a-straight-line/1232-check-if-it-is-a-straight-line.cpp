0c17e1e9427488c630bf5e3be826931ea820df2b
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-06-05 07:57:25 +0530

    Time: 8 ms (73.14%), Space: 9.9 MB (91.90%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -6,23 +6,23 @@ public:
         double x1=coordinates[1][0],y1=coordinates[1][1];
         bool isVertical=(x1==x0);
         double slope;
-        if (not isVertical)
+        if(not isVertical)
         {
             slope=(y1-y0)/(x1-x0);
         }
 
-        for(int i=2; i<n; i++)
+        for(int i=2;i<n;++i)
         {
-            double x=coordinates[i][0], y=coordinates[i][1];
-            if (isVertical)
+            double x=coordinates[i][0],y=coordinates[i][1];
+            if(isVertical)
             {
-                if (x!=x0)
+                if(x!=x0)
                 {
                      return 0;
                 }
                 continue;
             } 
-            else if ((y-y0)!=(x-x0)*slope)
+            else if((y-y0)!=(x-x0)*slope)
             {
                 return 0;
             }
