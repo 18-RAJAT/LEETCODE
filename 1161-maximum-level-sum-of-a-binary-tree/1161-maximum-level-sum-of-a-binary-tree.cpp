959bdd719a2f2d92f05c55efab8ac2f7665a0d39
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-06-15 06:22:10 +0530

    Time: 196 ms (81.08%), Space: 107.4 MB (13.54%) - LeetHub

---
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        if(root==nullptr)
+        {
+            return 0;
+        }
+        int res=0,level=0,sum=INT_MIN;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int sza=q.size();
+            int tempSum=0;
+            while(sza--)
+            {
+                TreeNode*tmp=q.front();
+                q.pop();
+                tempSum+=tmp->val;
+                if(tmp->left)
+                {
+                    q.push(tmp->left);
+                }
+                if(tmp->right)
+                {
+                    q.push(tmp->right);
+                }
+            }
+            level++;
+            if(sum<tempSum)
+            {
+                res=level;
+            }
+            sum=max<int>(sum,tempSum);
+        }
+        return res;
+    }
+};
