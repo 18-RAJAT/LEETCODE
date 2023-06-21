8eb0707d7a9df16403d840339e509837242601ed
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-06-21 09:16:08 +0530

    Time: 149 ms (15.33%), Space: 41.7 MB (42.34%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
-
-    long long calc(vector<int>&nums, vector<int>& cost, long long median){
-
-        long long ans = 0;
-
-         for(int i=0;i<nums.size();i++)
-           ans += abs(1ll*nums[i]-median)*(1ll*cost[i]);
-
-         return ans;  
+    long long mod(int x){
+        if(x>0)return x;
+        return -x;
     }
+    
+    long long minCost(vector<int>& nums, vector<int>& costs) {
+        auto findCost=[&](long long pivot) -> long long{
+            long long ans=0;
+            for(int j=0;j<nums.size();j++){
+                long long diff = mod(nums[j]-pivot);
+                ans+=diff*(long long)costs[j];     
+            }
 
-    long long minCost(vector<int>& nums, vector<int>& cost) {
-
-        long long tot = 0;
-        long long sum = 0;
-        int n = nums.size();
-
-        vector<pair<int,int>> vec;
-
-        for(int i=0;i<nums.size();i++)
-            vec.push_back({nums[i],cost[i]});
-
-        sort(vec.begin(),vec.end());
-
-        for(int i=0;i<n;i++)
-        sum += vec[i].second;
-
-        long long median;
-        int i=0;
-
-        while(tot < (sum+1)/2 && i<n){
-
-             tot += 1ll*vec[i].second;
-             median = vec[i].first;
-             i++;
-        }    
+            return ans;
+        };
         
+        vector<pair<int,int>>arr;
+        int n=nums.size();
+        for(int i=0;i<n;i++)arr.push_back({nums[i],costs[i]});
+        sort(arr.begin(),arr.end());
+        int low=0, high=n-1;
         
-        return calc(nums,cost,median);
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if(findCost(arr[mid].first)< findCost(arr[mid].first +1))high=mid;
+            else low=mid+1;
+        }
+        
+        return findCost(arr[low].first);
     }
 };
