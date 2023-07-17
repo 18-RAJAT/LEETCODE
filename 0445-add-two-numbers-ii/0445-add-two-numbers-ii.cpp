0ea56be5a1108e9ef8cdfc84cc7c8ef27e0b259b
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-07-17 09:56:09 +0530

    Time: 57 ms (7.78%), Space: 73.6 MB (22.79%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -8,75 +8,62 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution
-{
-    public:
-        ListNode* reverseLL(ListNode *head)
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        stack<ListNode*>stack1;
+        stack<ListNode*>stack2;
+        
+        //L2 list k lie
+        while(l1)
         {
-            ListNode *curr = head;
-            ListNode *prev = NULL;
-            ListNode *forward = NULL;
-
-            while (curr)
-            {
-                forward = curr->next;
-                curr->next = prev;
-                prev = curr;
-                curr = forward;
-            }
-            return prev;
+            stack1.push(l1);
+            l1 = l1->next;
         }
-    ListNode* insert_tail(ListNode *head, int val)
-    {
-        ListNode *ptr = new ListNode(val);
-        ListNode *temp = head;
-        if (head == NULL)
+        
+        //L2 list k lie
+        
+        while(l2)
         {
-            head = ptr;
-            return head;
+            stack2.push(l2);
+            l2 = l2->next;
         }
-        while (temp->next)
-        {
-            temp = temp->next;
-        }
-        temp->next = ptr;
-        return head;
-    }
-    ListNode* adding(ListNode *l1, ListNode *l2)
-    {
-        ListNode *ans = NULL;
+        
+        //Firstly head will be initilize NULL 
+        
+        ListNode* head = NULL;
+        
+        //Taking a carry
         int carry = 0;
-        int sum = l1->val + l2->val;
-        while (l1 || l2 || carry)
+        
+        //Then entering the while loop
+        while(carry or !stack1.empty() or !stack2.empty())
         {
-            int val1 = 0, val2 = 0;
-            if (l1)
-                val1 = l1->val;
-            if (l2)
-                val2 = l2->val;
-            sum = val1 + val2 + carry;
-
-            ans = insert_tail(ans, sum % 10);
-            carry = sum / 10;
-
-            if (l1)
-                l1 = l1->next;
-            if (l2)
-                l2 = l2->next;
+            //when stack empty
+            if(!stack1.empty())
+            {
+                //then stack k top k sath add then pop
+                carry += stack1.top()->val,stack1.pop();
+            }
+            
+            //same as stack 2 k sath 
+            if(!stack2.empty())   
+            {
+                //carry add then pop
+                carry += stack2.top()->val,stack2.pop();
+            }
+            
+            //generate carry....!!
+            ListNode* node = new ListNode(carry % 10);
+            
+            node->next = head;
+            
+            head = node;
+            
+            carry = carry / 10;
+            
         }
-        return ans;
-    }
-    ListNode* addTwoNumbers(ListNode *l1, ListNode *l2)
-    {
-       	//step 1
-        l1 = reverseLL(l1);
-        l2 = reverseLL(l2);
-
-       	//step 2
-        ListNode *ans = adding(l1, l2);
-
-       	//step 3
-        ans = reverseLL(ans);
-        return ans;
+        return head;
     }
 };
