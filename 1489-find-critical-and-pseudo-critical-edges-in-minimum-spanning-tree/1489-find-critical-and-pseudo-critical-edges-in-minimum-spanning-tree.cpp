1ff6cbab03775344739f66406d0baa8b4910ffe0
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-08-19 21:37:05 +0530

    Time: 165 ms (57.63%), Space: 25 MB (42.37%) - LeetHub

--- 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
+++ 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
@@ -1,100 +1,71 @@
+class disjointset{
+    public:
+    vector<int>par, rank;
+    disjointset(int n){
+        par.resize(n+1);
+        rank.resize(n+1, 0);
+        for(int i=0; i<=n; i++){
+            par[i]=i;
+        }
+    }
+    int findp(int u){
+        if(u==par[u]){return u;}
+        return par[u]=findp(par[u]);
+    }
+    void un(int u, int v){
+        int pu=findp(u), pv=findp(v);
+        if(pu==pv){return;}
+        if(rank[pu]<rank[pv]){
+            par[pu]=pv;
+        }
+        else{
+            par[pv]=pu;
+            rank[pu]++;
+        }
+    }
+};
 class Solution {
 public:
-    class UnionFind {
-    public:
-        vector<int> parent;
-        vector<int> size;
-        int maxSize;
-
-        UnionFind(int n) {
-            parent.resize(n);
-            size.resize(n, 1);
-            maxSize = 1;
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
+    int mst(int n, vector<pair<int, pair<int, int>>>& v, pair<int, pair<int, int>>a, pair<int, pair<int, int>>b){
+        disjointset d(n);
+        int ans=0;
+        if(a.second.first!=-1 && a.second.second!=-1){d.un(a.second.first, a.second.second); ans+=a.first;}
+        //if(b.second.first!=-1 && b.second.second!=-1){d.un(b.second.first, b.second.second);}
+        for(auto it: v){
+            if(b!=it && (d.findp(it.second.first)!=d.findp(it.second.second))){
+                ans+=it.first;
+                d.un(it.second.first, it.second.second);
             }
         }
-
-        int find(int x) {
-            // Finds the root of x
-            if (x != parent[x]) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
-        }
-
-        bool unite(int x, int y) {
-            // Connects x and y
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX != rootY) {
-                if (size[rootX] < size[rootY]) {
-                    swap(rootX, rootY);
-                }
-                parent[rootY] = rootX;
-                size[rootX] += size[rootY];
-                maxSize = max(maxSize, size[rootX]);
-                return true;
-            }
-            return false;
-        }
-    };
-
+        set<int>s;
+        for(int i=0; i<n; i++){s.insert(d.findp(i));}
+        if(s.size()==1){return ans;}
+        return INT_MAX;
+    }
     vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
-        auto newEdges = edges;
-        // Add index to edges for tracking
-        int m = newEdges.size();
-        for (int i = 0; i < m; i++) {
-            newEdges[i].push_back(i);
+        vector<pair<int, pair<int, int>>>v;
+        vector<vector<int>>ans;
+        vector<int>c, p;
+        int i=0;
+        map<pair<int, int>, int>m;
+        for(auto it: edges){
+            v.push_back({it[2], {it[0], it[1]}});
+            m[{it[0], it[1]}]=i;
+            i++;
         }
-
-        // Sort edges based on weight
-        sort(newEdges.begin(), newEdges.end(), [](auto& a, auto& b) {
-            return a[2] < b[2];
-        }); 
-
-        // Find MST weight using union-find
-        UnionFind ufStd(n);
-        int stdWeight = 0;
-        for (const auto& edge : newEdges) {
-            if (ufStd.unite(edge[0], edge[1])) {
-                stdWeight += edge[2];
+        sort(v.begin(), v.end());
+        int sum=mst(n, v, {-1, {-1, -1}}, {-1, {-1, -1}});
+        //cout<<sum<<endl;
+        for(auto it: v){
+            int s1=mst(n, v, {-1, {-1, -1}}, it), s2=mst(n, v, it, {-1, {-1, -1}});
+            cout<<sum<<" "<<s1<<" "<<s2<<endl;
+            if(s1>sum){c.push_back(m[{it.second.first, it.second.second}]);}
+            else{
+                if(s2==sum){p.push_back(m[{it.second.first, it.second.second}]);}
             }
         }
-
-        vector<vector<int>> results(2);
-        // Check each edge for critical and pseudo-critical
-        for (int i = 0; i < m; i++) {
-            UnionFind ufIgnore(n);
-            int ignoreWeight = 0;
-            for (int j = 0; j < m; j++) {
-                if (i != j && ufIgnore.unite(newEdges[j][0], newEdges[j][1])) {
-                    ignoreWeight += newEdges[j][2];
-                }
-            }
-
-            // If the graph is disconnected or the total weight is greater, 
-            // the edge is critical
-            if (ufIgnore.maxSize < n || ignoreWeight > stdWeight) {
-                results[0].push_back(newEdges[i][3]);
-            } else {
-                // Force this edge and calculate MST weight
-                UnionFind ufForce(n);
-                ufForce.unite(newEdges[i][0], newEdges[i][1]);
-                int forceWeight = newEdges[i][2];
-                for (int j = 0; j < m; j++) {
-                    if (i != j && ufForce.unite(newEdges[j][0], newEdges[j][1])) {
-                        forceWeight += newEdges[j][2];
-                    }
-                }
-
-                // If total weight is the same, the edge is pseudo-critical
-                if (forceWeight == stdWeight) {
-                    results[1].push_back(newEdges[i][3]);
-                }
-            }
-        }
-
-        return results;
+        ans.push_back(c);
+        ans.push_back(p);
+        return ans;
     }
 };
