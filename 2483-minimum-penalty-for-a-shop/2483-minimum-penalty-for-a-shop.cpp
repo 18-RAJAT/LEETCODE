5c89983454fa7a797e8888fb12e7a6a0791d8a4a
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-08-29 09:11:37 +0530

    Time: 14 ms (99.36%), Space: 10.8 MB (72.59%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int bestClosingTime(string s) {
+        int n=s.size(),count=0,ans=n;
+        for(int i=n-1;i>=0;--i)
+        {
+            count+=(s[i]=='Y')-(s[i]=='N');
+            if(count<=0)
+            {
+                ans=i;
+            }
+            if(count<0)
+            {
+                count=0;
+            }
+        }
+        return ans;
+    }
+};
