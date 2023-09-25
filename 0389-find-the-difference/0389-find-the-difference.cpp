67285e2215677cad7e2075791f733cefd56a9252
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-09-25 10:13:02 +0530

    Time: 0 ms (100.00%), Space: 7 MB (8.68%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        int ans=0;
+        for(int i=0;i<n;++i)
+        {
+            ans^=s[i];
+        }
+        for(int i=0;i<m;++i)
+        {
+            ans^=t[i];
+        }
+        return ans;
+    }
+};
