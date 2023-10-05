40149469cc1393553178cb183ddb3c8622b30e1d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-10-05 15:40:51 +0530

    Time: 11 ms (47.19%), Space: 7.7 MB (15.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int>freq(26,0);
-        if(s.length()!=t.length())
-        {
-            return false;
-        }
-        //store freq of char's in s1 and s2
-        for(int i=0;i<s.length();++i)
-        {
-            freq[s[i]-'a']++;
-            freq[t[i]-'a']--;
-        }
-        //checking if freq of every char's is 0
-        for(int i=0;i<26;++i)
-        {
-            if(freq[i]!=0)
-            {
-                return false;
-            }
-        }
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t?true:false;
     }
 };
