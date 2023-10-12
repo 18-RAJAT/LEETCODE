187084acbf3dd56307ffac4e64a4e204bc70043a
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-10-12 19:28:03 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (32.06%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -3,20 +3,24 @@
 
 class Solution {
 public:
-    int firstBadVersion(int n) {
-        int left=1,right=n;
-        while(left<=right)
+    int binarySearch(int left,int right)
+    {
+        if(left==right)
         {
-            int mid=left+(right-left)/2;
-            if(isBadVersion(mid))
-            {
-                right=mid-1;
-            }
-            else
-            {
-                left=mid+1;
-            }
+            return left;
         }
-        return left;
+        int mid=left+(right-left)/2;
+        if(isBadVersion(mid))//if mid is bad then all the versions after mid are bad so we search in the left half of the array
+        {
+            return binarySearch(left,mid);
+        }
+        else
+        {
+            return binarySearch(mid+1,right);
+        }
+        return -1;
+    }
+    int firstBadVersion(int n) {
+        return binarySearch(1,n);
     }
 };
