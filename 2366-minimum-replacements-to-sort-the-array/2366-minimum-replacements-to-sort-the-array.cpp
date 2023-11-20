7ece14fd466de7f9337ebb385430d7f4f675c9b0
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-11-20 12:04:53 +0530

    Time: 88 ms (79.10%), Space: 55 MB (59.35%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     long long minimumReplacement(vector<int>& nums) {
+        // https://codeforces.com/contest/1898/submission/233445630
+        long long n=nums.size();
+        long long last=nums[n-1];
         long long ans=0;
-       for(int i=nums.size()-1;i>0;--i)
-       {
-           if(nums[i]<nums[i-1])
-           {
-               if(nums[i-1]%nums[i])
-               {
-                   ans+=nums[i-1]/nums[i];
-                   int div=nums[i-1]/nums[i]+1;
-                   nums[i-1]/=div;
-               }
-               else
-               {
-                  ans+=nums[i-1]/nums[i]-1; 
-                  nums[i-1]=nums[i];
-               }
-           }
-       }
-       return ans;
+        for(long long i=n-2;i>=0;--i)
+        {
+            if(nums[i]<=last)
+            {
+                last=nums[i];
+                continue;
+            }
+            if(nums[i]%last==0)
+            {
+                ans+=nums[i]/last-1;
+                // last=v[i]/last;
+                continue;
+            }
+            ans+=nums[i]/last;
+            last=nums[i]/(1+nums[i]/last);
+        }
+        return ans;
     }
 };
