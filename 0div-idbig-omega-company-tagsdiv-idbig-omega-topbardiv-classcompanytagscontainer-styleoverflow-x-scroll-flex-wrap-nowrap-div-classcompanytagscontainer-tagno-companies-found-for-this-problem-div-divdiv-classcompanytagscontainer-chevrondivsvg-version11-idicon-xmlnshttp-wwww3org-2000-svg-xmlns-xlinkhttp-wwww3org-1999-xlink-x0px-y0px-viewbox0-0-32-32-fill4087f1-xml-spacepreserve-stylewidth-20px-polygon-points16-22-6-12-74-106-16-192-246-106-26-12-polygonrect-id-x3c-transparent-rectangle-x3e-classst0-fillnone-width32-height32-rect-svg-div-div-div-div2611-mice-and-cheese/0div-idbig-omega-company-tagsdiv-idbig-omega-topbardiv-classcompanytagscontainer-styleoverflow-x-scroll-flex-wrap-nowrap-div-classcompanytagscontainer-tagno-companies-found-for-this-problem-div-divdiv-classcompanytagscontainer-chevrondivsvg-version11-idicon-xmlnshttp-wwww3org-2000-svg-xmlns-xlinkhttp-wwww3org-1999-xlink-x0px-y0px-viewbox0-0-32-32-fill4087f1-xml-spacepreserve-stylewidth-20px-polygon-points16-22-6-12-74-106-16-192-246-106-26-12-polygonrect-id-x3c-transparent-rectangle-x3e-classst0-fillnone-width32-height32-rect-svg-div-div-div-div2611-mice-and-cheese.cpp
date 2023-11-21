11310ebfcc46e235a5b18f748ad1ce1c56ff4bbc
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-11-21 17:48:40 +0530

    Time: 351 ms (6.99%), Space: 128.9 MB (18.42%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2611-mice-and-cheese/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2611-mice-and-cheese.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
+        int n=reward1.size();
+        vector<vector<int>>arr(n,vector<int>(3,0));//0->ans,1->reward1,2->reward2
+        for(int i=0;i<n;++i)
+        {
+            int &ans=arr[i][0];//ans
+            int& R1=arr[i][1];//reward1
+            int& R2=arr[i][2];//reward2
+            
+            R1=reward1[i];
+            R2=reward2[i];
+            ans=R1-R2;
+        }
+        sort(arr.rbegin(),arr.rend());
+        int res=0;
+        for(int i=0;i<n;++i)
+        {
+            if(k>=1)
+            {
+                res+=arr[i][1];//reward1
+                k--;//mice eaten by cat 1
+            }
+            else
+            {
+                res+=arr[i][2];//reward2
+            }
+        }
+        return res;
+    }
+};
