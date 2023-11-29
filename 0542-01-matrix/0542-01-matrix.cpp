6b3350e5bfc2e5f1a0c6f579965c1973a56bf516
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-11-29 20:16:45 +0530

    Time: 68 ms (51.39%), Space: 32.1 MB (65.51%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,39 +3,40 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        vector<vector<int>>ans(n,vector<int>(m,INT_MAX));
-        queue<pair<int,int>>q;
-        for(int i=0;i<n;i++)
+        vector<vector<int>>answer(n,vector<int>(m,INT_MAX));
+        queue<pair<int,int>>que;
+        for(int i=0;i<n;++i)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<m;++j)
             {
                 if(mat[i][j]==0)
                 {
-                    ans[i][j]=0;
-                    q.push({i,j});
+                    answer[i][j]=0;
+                    que.emplace(make_pair(i,j));
                 }
             }
         }
-        vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
-        while(!q.empty())
+        int dx[4]={-1,0,1,0};
+        int dy[4]={0,1,0,-1};
+        while(!que.empty())
         {
-            int x=q.front().first;
-            int y=q.front().second;
-            q.pop();
-            for(auto d:dir)
+            pair<int,int>current=que.front();
+            que.pop();
+            for(int i=0;i<4;++i)
             {
-                int nx=x+d.first;
-                int ny=y+d.second;
-                if(nx>=0 && nx<n && ny>=0 && ny<m)
+                int row=current.first+dx[i];
+                int col=current.second+dy[i];
+                auto isValid=[&](int row,int col)->bool
                 {
-                    if(ans[nx][ny]>ans[x][y]+1)
-                    {
-                        ans[nx][ny]=ans[x][y]+1;
-                        q.push({nx,ny});
-                    }
+                    return(row>=0 && row<n && col>=0 && col<m)?true:false;
+                };
+                if(isValid(row,col) && answer[row][col]>=answer[current.first][current.second]+1)
+                {
+                    answer[row][col]=1+answer[current.first][current.second];
+                    que.emplace(make_pair(row,col));
                 }
             }
         }
-        return ans;
+        return answer;
     }
 };
