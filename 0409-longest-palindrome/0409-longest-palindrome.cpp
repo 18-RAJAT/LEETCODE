ba38cf575a0367b296aad93136f2bf1cad840ea5
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2023-11-29 10:25:02 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (99.25%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n=s.size();
+        int a[26];
+        memset(a,0,sizeof(a));
+        int b[26];
+        memset(b,0,sizeof(b));
+        
+        auto convert=[&](int i)->void
+        {
+            if(s[i]>='a' && s[i]<='z')
+            {
+                a[s[i]-'a']++;
+            }
+            else
+            {
+                b[s[i]-'A']++;
+            }
+        };
+        
+        for(int i=0;i<n;++i)
+        {
+            convert(i);
+        }
+        long long int ans=0;
+        bool check=false;
+        for(int i=0;i<26;++i)
+        {
+            if(a[i]&1)
+            {
+                check=true;
+            }
+            ans+=a[i]/2;
+        }
+        for(int i=0;i<26;++i)
+        {
+            if(b[i]&1)
+            {
+                check=true;
+            }
+            ans+=b[i]/2;
+        }
+        if(check)
+        {
+            return 2*ans+1;
+        }
+        else
+        {
+            return 2*ans;
+        }
+    }
+};
