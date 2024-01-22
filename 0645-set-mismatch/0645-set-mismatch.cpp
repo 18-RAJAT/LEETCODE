d871436aede8a4abac419635556dae1eb69ab0a7
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-01-22 06:17:22 +0530

    Time: 16 ms (95.15%), Space: 23.8 MB (36.81%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,43 +1,19 @@
-#define MOD 1000000007
-#define PI 3.1415926535897932384626433832795
-#define ll long long
-#define all(x) sort(x.begin(), x.end())
-#define ff(a,b) for(int i=a;i<b;i++)
-#define f1(i,s,e) for(long long int i=s;i<e;i++)
-#define cf(i,s,e) for(long long int i=s;i<=e;i++)
-#define FOREACH(n) for(auto it:n)
-#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
-#define pb push_back
-#define eb emplace_back
-typedef pair<int, int> PII;
-typedef vector<int> vi;
-typedef vector<bool> vb;
-typedef vector<vector<int>> VII;
-typedef vector<string> VS;
-typedef vector<vector<string>> VVS;
-typedef vector<PII> VIII;
-typedef vector<vi> VVI;
-typedef map<int,int> MPII;
-typedef priority_queue<pair<int,pair<int,int>>> PQVI;
-typedef unordered_map<string,vector<string>> MPSVS;
-
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n=nums.size();
-        vector<bool>bit(n+1,false);
-        
-        vector<int>ans;
-        for(auto n:nums)
+        vector<int>ans(2);
+        for(int i=0;i<n;i++)
         {
-            if(bit[n]==true)
-                ans.pb(n);
-            bit[n]=true;
+            (nums[abs(nums[i])-1]<0)?ans[0]=abs(nums[i]):nums[abs(nums[i])-1]*=-1;
         }
-        cf(i,1,n)
+        for(int i=0;i<n;i++)
         {
-            if(bit[i]==false)
-                ans.pb(i);
+            if(nums[i]>=1)
+            {
+                ans[1]=i+1;
+                break;
+            }
         }
         return ans;
     }
