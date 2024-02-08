99e13662b81b429766e3c66fa69972978d23d99d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-02-08 10:42:19 +0530

    Time: 33 ms (94.19%), Space: 10.4 MB (42.39%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,36 +1,15 @@
-#define f1(i,s,e) for(long long int i=s;i<e;i++)
-#define ff1(i,s,e) for(long long int i=s;i>=e;--i)
-#define For1(i,n) (long long int i=0;i<n+1;++i)
-#define cf(i,s,e) for(long long int i=s;i<=e;i++)
-
 class Solution {
 public:
     int numSquares(int n) {
-    
-        // int dp[n+1];
-        // memset(dp,INT_MAX,sizeof(dp));
-        
-        //pruning
         vector<int>dp(n+1,INT_MAX);
-        
-        //base case
         dp[0]=0;
-        
-        //cache check
-        cf(i,1,n)
+        for(int i=1;i<=n;++i)
         {
-            // long long int x=sqrt(n);
-            // cf(j,2,x)
-            for(int j=1;j*j<=i;j++)
+            for(int j=1;j*j<=i;++j)
             {
-                //compute
-                int sq=j*j;
-                dp[i]=min(dp[i],dp[i-sq]+1);
-                // cout<<dp[i]<<endl;
+                dp[i]=min(dp[i],dp[i-j*j]+1);
             }
         }
-        //save and return
         return dp[n];
-        // cout<<dp[n]<<endl;
     }
 };
