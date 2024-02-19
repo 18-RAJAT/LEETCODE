3956d7770f03ae9ec7c116c2d2c56e948d83914b
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-02-19 19:29:52 +0530

    Time: 49 ms (5.16%), Space: 30.6 MB (5.01%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,54 +1,19 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        // vector<int>cnt(100000,0);
-        // int n=nums.size();
-        // for(int i=0;i<n;++i)
-        // {
-        //     cnt[nums[i]]++;
-        // }
-        // for(int i=0;i<cnt.size();++i)
-        // {
-        //     if(cnt[i]==1)
-        //     {
-        //         return i;
-        //     }
-        // }
-        // return 0;
-        
-        
-        
-        //2nd approach
-        function<int(int,int)>binarySearch=[&](int left,int right)
+        int n=nums.size();
+        map<int,int>mp;
+        for(auto& it:nums)
         {
-            if(left==right)
+            mp[it]++;
+        }
+        for(auto& [key,value]:mp)
+        {
+            if(value==1)
             {
-                return nums[left];
+                return key;
             }
-            int mid=left+(right-left)/2;
-            if(mid%2==0)
-            {
-                if(nums[mid]==nums[mid+1])
-                {
-                    return binarySearch(mid+2,right);
-                }
-                else
-                {
-                    return binarySearch(left,mid);
-                }
-            }
-            else
-            {
-                if(nums[mid]==nums[mid-1])
-                {
-                    return binarySearch(mid+1,right);
-                }
-                else
-                {
-                    return binarySearch(left,mid-1);
-                }
-            }
-        };
-        return binarySearch(0,nums.size()-1);
+        }
+        return -1;
     }
 };
