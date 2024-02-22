8279a7247974d2ed2c8d3d2cf3ec5967cc060c10
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-02-22 09:43:58 +0530

    Time: 128 ms (75.86%), Space: 64.3 MB (62.99%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        int in[n+1];memset(in,0,sizeof(in));
-        int out[n+1];memset(out,0,sizeof(out));
-        
-        for(auto &it:trust)
+        vector<int>in(n+1,0);
+        vector<int>out(n+1,0);
+        for(int i=0;i<trust.size();++i)
         {
-            in[it[1]]++;
-            out[it[0]]++;
+            in[trust[i][1]]++;
+            out[trust[i][0]]++;
         }
         for(int i=1;i<=n;++i)
         {
-            if(out[i]==0 and in[i]==n-1)
+            if(in[i]==n-1 && out[i]==0)
             {
                 return i;
             }
