4c505703c4ba1f5d930e473bd0b82549159677ac
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-02-23 20:11:31 +0530

    Time: 15 ms (78.73%), Space: 15.8 MB (66.90%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,14 @@
-
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        auto bellmanFord=[&]()->int
-        {
-            vector<int>dist(n,INT_MAX);
-            dist[src]=0;//initial point
-            for(int i=0;i<=k;i++)
-            {
-                vector<int>dist1=dist;
-                for(auto &edge:flights)
-                {
-                    int u=edge[0];
-                    int v=edge[1];
-                    int w=edge[2];
-                    if(dist[u]!=INT_MAX)
-                    {
-                        dist1[v]=min(dist1[v],dist[u]+w);
-                    }
-                }
-                dist=dist1;
+        int inf = 1e9;
+        vector<vector<int>> dp(2, vector<int>(n, inf));
+        dp[0][src] = dp[1][src] = 0;
+        for (int i = 0; i <= k; i++) {
+            for (auto& flight : flights) {
+                dp[i % 2][flight[1]] = min(dp[i % 2][flight[1]], dp[(i + 1) % 2][flight[0]] + flight[2]);
             }
-            if(dist[dst]==INT_MAX)
-            {
-                return -1;
-            }
-            else
-            {
-                return dist[dst];
-            }
-        };
-        return bellmanFord();
+        }
+        return dp[k % 2][dst] >= inf ? -1 : dp[k % 2][dst];
     }
 };
