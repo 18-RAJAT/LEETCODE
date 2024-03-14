34b5f27845338b3cb1156de5c1ce75091056c05d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-03-14 10:58:25 +0530

    Time: 43 ms (43.99%), Space: 38.3 MB (29.75%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,19 +1,24 @@
 class Solution {
-    public:
-        int numSubarraysWithSum(vector<int>& nums, int goal) {
-            array<int,100005>count{};
-            count[0]=1;//to handle the case when sum-goal=0
-            int allPossibleCount=0;//to store the count of all possible subarrays
-            int sum=0;
-            for(auto num:nums)
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n=nums.size();
+        int ans=0;
+        int sum=0;
+        unordered_map<int,int>mp;
+        mp.clear();
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            if(sum==goal)
             {
-                sum+=num;//calculating the sum of subarray
-                if(goal<=sum)//if sum is greater than or equal to goal then we will check if there is any subarray with sum equal to sum-goal
-                {
-                    allPossibleCount+=count[sum-goal];//if there is any subarray with sum equal to sum-goal then we will add it to our answer
-                }
-                count[sum]++;//increasing the count of sum
+                ans++;
             }
-            return allPossibleCount;
+            if(mp.count(sum-goal))
+            {
+                ans+=mp[sum-goal];
+            }
+            mp[sum]++;
         }
-    };
+        return ans;
+    }
+};
