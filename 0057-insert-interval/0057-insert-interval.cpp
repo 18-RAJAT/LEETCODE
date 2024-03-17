236fc769fef1060602e95a3269985996a6208121
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-03-17 09:55:42 +0530

    Time: 9 ms (76.06%), Space: 20.4 MB (58.08%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    //in=interval
-    //inN=newInterval
-    vector<vector<int>> insert(vector<vector<int>>& in,vector<int>& inN) {
-        vector<vector<int>> ans;
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>>ans;
         int i=0;
-        while(i<in.size() and in[i][1]<inN[0])
+        while(i<intervals.size() && intervals[i][1]<newInterval[0])//for non-overlapping intervals
         {
-            ans.push_back(in[i]);
-            ++i;
+            ans.push_back(intervals[i]);
+            i++;
         }
-        while(i<in.size() and in[i][0]<=inN[1])
+        while(i<intervals.size() && intervals[i][0]<=newInterval[1])//for overlapping intervals
         {
-            inN[0]=min(inN[0],in[i][0]);
-            inN[1]=max(inN[1],in[i][1]);
-            ++i;
+            newInterval[0]=min(newInterval[0],intervals[i][0]);//updating the start point of newInterval
+            newInterval[1]=max(newInterval[1],intervals[i][1]);//updating the end point of newInterval
+            i++;
         }
-        ans.push_back(inN);
-        while(i<in.size())
+        ans.push_back(newInterval);
+        while(i<intervals.size())//for non-overlapping intervals
         {
-            ans.push_back(in[i]);
-            ++i;
+            ans.push_back(intervals[i]);//pushing the remaining intervals
+            i++;
         }
         return ans;
     }
