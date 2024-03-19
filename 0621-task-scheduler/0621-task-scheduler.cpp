427dc3dd540ee994734a746627ef9d0c578ed24f
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-03-19 10:59:56 +0530

    Time: 52 ms (78.11%), Space: 38 MB (80.77%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int fz[26];
+        fill(fz,fz+26,0);
+        for(int i=0;i<tasks.size();++i)
+        {
+            fz[tasks[i]-'A']++;
+        }
+        sort(fz,fz+26);
+        int maxFz=fz[25];
+        int idleTime=(maxFz-1)*n;
+        for(int i=24;i>=0;--i)
+        {
+            idleTime-=min(maxFz-1,fz[i]);
+        }
+        return max((int)tasks.size(),(int)tasks.size()+idleTime);
+    }
+};
