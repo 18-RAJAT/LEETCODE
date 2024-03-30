7b1172a82e5913713fbd9af101cdce198be69dbc
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-03-30 12:11:04 +0530

    Time: 3 ms (40.25%), Space: 7.1 MB (97.47%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    int right_shift(int x)
-    {
-        return x>>1;
-    }
-    int bitwise_xor(int x,int y)
-    {
-        return x^y;
-    }
     int minimumOneBitOperations(int n) {
         int ans=0;
         while(n)
         {
-            ans=bitwise_xor(ans,n);
-            n=right_shift(n);
+            ans^=n;
+            n>>=1;
         }
         return ans;
     }
