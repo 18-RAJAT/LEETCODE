5076141e628c6bb4fab063c3be19f45ec0fff514
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-04-06 09:56:27 +0530

    Time: 5 ms (27.81%), Space: 10.6 MB (38.45%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        sort(nums.begin(),nums.end());
         int n=nums.size();
         int i=0,j=n-1;
         while(i<=j)
