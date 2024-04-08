a7e1a27fa81bdf38447a90ddc096a9d15725c38e
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-04-08 13:11:04 +0530

    Time: 32 ms (92.95%), Space: 12 MB (76.93%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,52 +1,22 @@
+
+
+
 class Solution {
 public:
+    int dp[1005][1005];
+    string s;
+    int lps(int i,int j)
+    {
+        if(i>j)return 0;
+        if(i==j)return 1;
+        int &ret=dp[i][j];
+        if(~ret)return ret;
+        if(s[i]==s[j])return ret=2+lps(i+1,j-1);
+        return ret=max(lps(i+1,j),lps(i,j-1));
+    }
     int longestPalindromeSubseq(string s) {
-        
-        int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        // function<int(int,int)>LPS=[&](int i,int j)->int
-        // {
-        //     if(i>j)
-        //     {
-        //         return 0;
-        //     }
-        //     if(i==j)
-        //     {
-        //         return 1;
-        //     }
-        //     //already compute
-        //     if(dp[i][j]!=0)
-        //     {
-        //         return dp[i][j];
-        //     }
-        //     if(s[i]==s[j])
-        //     {
-        //         int dpState=LPS(i+1,j-1);
-        //         return 2+dpState;
-        //     }
-        //     return max(LPS(i+1,j),LPS(i,j-1));
-        // };
-        // return LPS(0,s.size()-1);
-        
-        for(int i=0;i<n;++i)
-        {
-            dp[i][i]=1;
-        }
-        for(int i=n-1;i>=0;--i)
-        {
-            for(int j=i+1;j<n;++j)
-            {
-                if(s[i]==s[j])
-                {
-                    int dpState=dp[i+1][j-1];
-                    dp[i][j]=2+dpState;
-                }
-                else
-                {
-                    dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
-                }
-            }
-        }
-        return dp[0][n-1];
+        this->s=s;
+        fill(&dp[0][0],&dp[0][0]+1005*1005,-1);
+        return lps(0,s.size()-1);
     }
 };
