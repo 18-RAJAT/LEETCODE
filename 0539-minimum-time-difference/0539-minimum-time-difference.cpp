f084ba5285070e0b47f357948d1258f47d798560
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-09-16 13:48:05 +0530

    Time: 23 ms (6.62%), Space: 17.3 MB (98.24%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -3,7 +3,8 @@ public:
     int findMinDifference(vector<string>& timePoints) {
         int minDiff=INT_MAX;
         sort(timePoints.begin(),timePoints.end());
-        for(int i=0;i<timePoints.size();i++){
+        for(int i=0;i<timePoints.size();++i)
+        {
             int h1=stoi(timePoints[i].substr(0,2)),m1=stoi(timePoints[i].substr(3,2));
             int h2=stoi(timePoints[(i+1)%timePoints.size()].substr(0,2)),m2=stoi(timePoints[(i+1)%timePoints.size()].substr(3,2));
             int diff=(h2-h1)*60+(m2-m1);
