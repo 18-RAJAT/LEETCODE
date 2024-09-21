8d760f048e8b159ad47b1ef3e89aa521ceab69f6
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-09-21 10:18:58 +0530

    Time: 7 ms (75.99%), Space: 14.1 MB (49.01%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,35 +1,16 @@
-typedef vector<int> vi;
-#define fl(a,b) for(int i=a;i<b;i++)
-#define pb push_back
-
 class Solution {
 public:
-    void helper(vi& result,int n,int tp)
+    void recur(vector<int>& result,int n,int nums)
     {
-        // for(int i=0;i<10;++i)
-        // {
-        //     int st=i+tp;
-        //     if(st>n and result.size()>=n){break;}
-        //     result.pb(st);
-        //     helper(result,n,st*10);
-        // }
-        if(tp>n)return;
-        result.pb(tp);
-        
-        helper(result,n,tp*10);
-        if(tp%10!=9)
-        {
-            helper(result,n,tp+1);
-        }
+        if(n<nums)return;
+        result.push_back(nums);
+        recur(result,n,nums*10);
+        if(nums%10!=9)recur(result,n,nums+1);
     }
-    
-        vi lexicalOrder(int n) {
-        vi result;
-        // if(n<=0){return result;}
-        // result.reserve(n);
-        // helper(result,n,1);
-        // return result;
-        helper(result,n,1);
+    vector<int>lexicalOrder(int n)
+    {
+        vector<int> result;
+        recur(result,n,1);
         return result;
     }
 };
