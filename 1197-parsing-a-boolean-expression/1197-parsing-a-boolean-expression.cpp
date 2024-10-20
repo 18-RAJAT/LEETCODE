ccfeeebb5e1164725e410ef4c38424114a68f54d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-10-20 12:40:11 +0530

    Time: 0 ms (100%), Space: 9.7 MB (75.94%) - LeetHub

---
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        stack<char> s;
+        int sza=expression.size();
+        for(int i=0;i<sza;++i)
+        {
+            if(expression[i] != ')')
+            {
+                s.push(expression[i]);
+            }
+            else
+            {    
+                int And=1,Or=0;
+                while(!s.empty())
+                {
+                    char ch=s.top();
+                    s.pop();
+                    if(ch=='f')
+                    {
+                        And&=0;
+                        Or|=0;
+                    }
+                    else if(ch=='t')
+                    {
+                        And &= 1;
+                        Or |= 1;
+                    }
+                    else if(ch=='&')
+                    {
+                        s.push(And?'t':'f');
+                        break;
+                    }
+                    else if(ch=='|')
+                    {
+                        s.push(Or?'t':'f');
+                        break;
+                    }
+                    else if(ch=='!')
+                    {
+                        s.push(!Or?'t':'f');
+                        break;
+                    }
+                }
+            }
+        }
+        return s.top()=='f'?0:1;
+    } 
+};
