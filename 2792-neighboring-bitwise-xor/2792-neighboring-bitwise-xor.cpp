e47abf13ea242f407338159958b994ceb535865c
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-01-17 08:21:34 +0530

    Time: 4 ms (47.66%), Space: 264.1 MB (28.04%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int ans=0;
+        for(int i=0;i<derived.size();++i)
+        {
+            ans+=derived[i];
+        }
+        return ans&1?0:1;
+    }
+};
