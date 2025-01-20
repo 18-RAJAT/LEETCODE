29a89fe709e0c519f94638ee11e06002f5a627d4
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-01-20 07:15:28 +0530

    Time: 224 ms (14.02%), Space: 164.6 MB (65.24%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int row=mat.size();
-        int col=mat[0].size();
+        int row=mat.size(),col=mat[0].size();
         int r[row];memset(r,0,sizeof(r));
         int c[col];memset(c,0,sizeof(c));
-        map<int,pair<int,int>>m;
-        for(int i=0;i<row;i++)
+        map<int,pair<int,int>>mp;
+        for(int i=0;i<row;++i)
         {
-            for(int j=0;j<col;j++)
+            for(int j=0;j<col;++j)
             {
-                m.insert(make_pair(mat[i][j],make_pair(i,j)));
+                mp.insert(make_pair(mat[i][j],make_pair(i,j)));
             }
         }
-        for(int i=0;i<arr.size();i++)
+        for(int i=0;i<arr.size();++i)
         {
-            int x=m[arr[i]].first;
-            int y=m[arr[i]].second;r[x]++;c[y]++;
-            if(c[y]==row)
-            {
-                return i;
-            }
-            if(r[x]==col)
-            {
-                return i;
-            }
+            int x=mp[arr[i]].first,y=mp[arr[i]].second;
+            r[x]++;c[y]++;
+            if(c[y]==row)return i;
+            if(r[x]==col)return i;
         }
         return arr.size()-1;
     }
