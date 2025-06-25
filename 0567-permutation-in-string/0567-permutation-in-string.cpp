d7776bcbf1fe499af10e3dd94bce06720303b653
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-06-26 01:24:15 +0530

    Time: 534 ms (9.42%), Space: 57.6 MB (10.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
-    bool compute(int a[26],int b[26])
-    {
-        for(int i=0;i<26;++i)
-        {
-            if(a[i]!=b[i])return false;
-        }
-        return true;
-    }
     bool checkInclusion(string s1, string s2) {
-        int n=s1.length(),m=s2.length();
-        if(n>m)return false;
-        int cnt1[26];
-        memset(cnt1,0,sizeof(cnt1));
-        for(int i=0;i<n;++i)
+        int n=s1.size(),m=s2.size();
+        if(m<n)return false;
+        unordered_map<char,int>mp,mp1;
+        for(auto& it:s1)mp[it]++;
+        // for(int i=0;i+n-1<m;++i)
+        for(int i=0;i<=m-n;++i)
         {
-            int idx1=s1[i]-'a';
-            cnt1[idx1]++;
-        }
-        int i=0,window=n;
-        int cnt2[26];
-        memset(cnt2,0,sizeof(cnt2));
-        while(i<window)
-        {
-            int idx2=s2[i]-'a';
-            cnt2[idx2]++,i++;
-        }
-        if(compute(cnt1,cnt2))return true;
-        while(i<m)
-        {
-            char currPos=s2[i];
-            int idx=currPos-'a';
-            cnt2[idx]++;
-            
-            char prev=s2[i-window];
-            idx=prev-'a';
-            cnt2[idx]--,i++;
-            
-            if(compute(cnt1,cnt2))return true;
+            mp1.clear();
+            int j=i;
+            while(j<=i+n-1)
+            {
+                mp1[s2[j]]++,j++;
+            }
+            if(mp==mp1)return true;
         }
         return false;
     }
