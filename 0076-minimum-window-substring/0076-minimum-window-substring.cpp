be2be8c3b40308ce9722108c73b1e72fa3605b1c
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-06-26 00:34:21 +0530

    Time: 15 ms (40.65%), Space: 11.3 MB (84.24%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,48 +1,27 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        if(n<m)
+        //varibale size(Shrink-expand based on count match)... | hash map + sliding window shrink
+        int n=s.size(),m=t.size();
+        map<int,int>mp;
+        for(int i=0;i<m;++i)mp[t[i]]++;
+        int l=0,r=0,mn=INT_MAX,cnt=0,idx=-1;
+        while(r<n)
         {
-            return "";
-        }
-        vector<int>mp(256,0);
-        for(int i=0;i<m;++i)
-        {
-            mp[t[i]]++;
-        }
-        int start=0;
-        int end=0;
-        int min_len=INT_MAX;
-        int count=0;
-        int start_index=-1;
-        for(int i=0;i<n;++i)
-        {
-            mp[s[i]]--;
-            if(mp[s[i]]>=0)
+            if(mp[s[r]]>=1)cnt++;
+            mp[s[r]]--;
+            while(cnt==t.size())
             {
-                count++;
-            }
-            if(count==m)
-            {
-                while(mp[s[start]]<0)
+                if(r-l+1<mn)
                 {
-                    mp[s[start]]++;
-                    start++;
+                    idx=l,mn=r-l+1;
                 }
-                int len=i-start+1;
-                if(len<min_len)
-                {
-                    min_len=len;
-                    start_index=start;
-                }
+                mp[s[l]]++;
+                if(mp[s[l]]>=1)cnt--;
+                l++;
             }
+            r++;
         }
-        if(start_index==-1)
-        {
-            return "";
-        }
-        return s.substr(start_index,min_len);
+        return idx==-1?"":string(s.begin()+idx,s.begin()+idx+mn);
     }
 };
