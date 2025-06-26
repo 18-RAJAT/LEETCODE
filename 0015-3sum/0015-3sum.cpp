5cf0012468efeadc41805081f861c401e9083f4b
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-06-27 00:34:04 +0530

    Time: 60 ms (28.18%), Space: 29.1 MB (51.94%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        function<int(int,int,int)>binary_search=[&](int left,int right,int key)->int
+        vector<vector<int>>ans;
+        for(int i=0;i<n;++i)
         {
-            while(left<=right)
+            if(i>=1 && nums[i]==nums[i-1])continue;
+            int j=i+1,k=n-1;
+            while(j<k)
             {
-                int mid=left+(right-left)/2;
-                if(nums[mid]==key)
-                {
-                    return mid;
-                }
-                else if(nums[mid]>key)
-                {
-                    right=mid-1;
-                }
-                else
-                {
-                    left=mid+1;
-                }
-            }
-            return -1;
-        };
-        for(int i=0;i<n;++i)//first number
-        {
-            if(i>0 && nums[i]==nums[i-1])//duplicate
-            {
-                continue;
-            }
-            for(int j=i+1;j<n;++j)//second number
-            {
-                if(j>i+1 && nums[j]==nums[j-1])//dupicate
-                {
-                    continue;
-                }
-                int third=-(nums[i]+nums[j]);//third number
-                int k=binary_search(j+1,n-1,third);
-                if(~k)
+                if(nums[i]+nums[j]+nums[k]==0)
                 {
                     ans.push_back({nums[i],nums[j],nums[k]});
+                    int prevJ=nums[j],prevK=nums[k];
+                    while(j<k && nums[j]==prevJ)j++;
+                    while(j<k && nums[k]==prevK)k--;
                 }
+                else if(nums[i]+nums[j]+nums[k]<0)j++;
+                else k--;
             }
         }
         return ans;
