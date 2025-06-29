198dba19d9a4d8bf44c352827885a3e495feba9d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-06-30 01:22:27 +0530

    Time: 43 ms (15.97%), Space: 91.8 MB (6.22%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -2,28 +2,16 @@ class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         int n=nums.size();
-        vector<int> a(n);
-        for(int i=0;i<n;++i)
-        {
-            a[i]=(nums[i]==0)?-1:1;
-        }
-        map<int,int> mp;
+        vector<int>a(n);
+        for(int i=0;i<n;++i)a[i]=(nums[i]==0)?-1:1;
+        map<int,int>mp;
         int sum=0,ans=0;
         for(int i=0;i<n;++i)
         {
             sum+=a[i];
-            if(sum==0)
-            {
-                ans=max(ans,i+1);
-            }
-            else if(mp.find(sum)!=mp.end())
-            {
-                ans=max(ans,i-mp[sum]);
-            }
-            else
-            {
-                mp[sum]=i;
-            }
+            if(sum==0)ans=max(ans,i+1);
+            else if(mp.count(sum))ans=max(ans,i-mp[sum]);
+            else mp[sum]=i;
         }
         return ans;
     }
