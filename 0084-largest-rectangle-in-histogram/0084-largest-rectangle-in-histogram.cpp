680900f3c2ceb0953fe36fefbf161b245f77a4dd
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-06-30 13:11:29 +0530

    Time: 25 ms (50.15%), Space: 79.4 MB (97%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        heights.push_back(-1);
         int n=heights.size();
-        stack<pair<int,int>>st;
-        int largest=0;
-        for(int i=0;i<n;++i)
+        stack<int>st;
+        int maxArea=0;
+        //all bars including one extra (i==n) for remaining stack
+        for(int i=0;i<=n;++i)
         {
-            int j=i;
-            while(!st.empty() && heights[i]<st.top().first)
+            //
+            while(!st.empty() && (i==n || heights[i]<=heights[st.top()]))
             {
-                auto& [F,S]=st.top();
+                int cur=heights[st.top()],nextSmaller=i;
                 st.pop();
-                largest=max(largest,F*(i-S));
-                j=S;
+                //stack empty -> no smaller bar to the left
+                int prevSmaller=(st.empty())?-1:st.top();
+                /*
+                    width=(r-l-1) calculate area...
+                    [2,1,5,6,2,3]
+                    let say processing height 5 at idx 2.
+                    prevSmaller=idx 1-> vals(1)
+                    nextSmaller=idx 4-> vals(2)
+                    width=4-1-1=2;
+                    area=height*wdith=5*2=10;
+                    spans bar=[5,6]...
+                */
+                int area=cur*(nextSmaller-prevSmaller-1);
+                maxArea=max(maxArea,area);
             }
-            st.push({heights[i],j});
+            st.push(i);
         }
-        return largest;
+        return maxArea;
     }
 };
