ec6eb3862563601447dd0da2cd2324a7561799d1
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-06-30 12:49:25 +0530

    Time: 28 ms (43.05%), Space: 139.1 MB (60.35%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,56 +1,19 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // monotonic-queue
-        
-        // deque<int>dq;
-        // vector<int>ans;
-        // int sz=size(nums);
-        // for(int i=0;i<sz;++i)
-        // {
-        //     while(not dq.empty() and dq.front()<=i-k)
-        //     {
-        //         dq.pop_front();
-        //     }
-        //     while(not dq.empty() and nums[dq.back()]<=nums[i])
-        //     {
-        //         dq.pop_back();
-        //     }
-        //     dq.push_back(i);
-        //     if(i>=k-1)
-        //     {
-        //         ans.push_back(nums[dq.front()]);
-        //     }
-        // }
-        // return ans;
-        
-        
-        
-        //Heap(Priority-queue)
-        auto cmp=[&](pair<int,int>a,pair<int,int>b)->int
-        {
-            return a.first<b.first;
-        };
+        int n=nums.size();
+        deque<int>dq;
         vector<int>ans;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(cmp)>pq(cmp);
-        int sz=nums.size();
-        for(int i=0;i<sz;++i)
+        for(int i=0;i<n;++i)
         {
-            if(i<k)
-            {
-                pq.emplace(nums[i],i);
-            }
-            else
-            {
-                ans.push_back(pq.top().first);
-                while(not pq.empty() and pq.top().second<=i-k)
-                {
-                    pq.pop();
-                }
-                pq.emplace(nums[i],i);
-            }
+            //larger ele...
+            while(!dq.empty() && nums[dq.back()]<nums[i])dq.pop_back();
+            dq.push_back(i);
+            //remove ele not in window
+            if(dq.front()<=i-k)dq.pop_front();
+            //add the max ele
+            if(k-1<=i)ans.push_back(nums[dq.front()]);            
         }
-        ans.push_back(pq.top().first);
         return ans;
     }
 };
