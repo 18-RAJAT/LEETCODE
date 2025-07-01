3290a461bb8038193adc668a6a244fcfbf8ac784
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-07-01 23:51:09 +0530

    Time: 4 ms (30.91%), Space: 22.2 MB (6.91%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
+    int find(int node,vector<int>& par)
+    {
+        if(par[node]==node)return node;
+        return par[node]=find(par[node],par);
+    }
+    void Union(int u,int v,vector<int>& par,vector<int>& size)
+    {
+        int x=find(u,par),y=find(v,par);
+        if(x==y)return;
+        if(size[x]<size[y])
+        {
+            par[x]=y;
+            size[y]+=size[x];
+        }
+        else
+        {
+            par[y]=x;
+            size[x]+=size[y];
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        
-        if(isConnected.empty())
-        {
-            return 0;
-        }
-        
-        int n = isConnected.size();
-        
-        vector<int>leads(n,0);
-        
+        int n=isConnected.size();
+        vector<int>par(n);
+        iota(par.begin(),par.end(),0LL);
+        vector<vector<int>>edges;
         for(int i=0;i<n;++i)
         {
-            leads[i] = i;
-        }
-        
-        int groups = n;
-        for(int i=0;i<n;++i)
-        {
-            for(int j=i+1;j<n;++j)
+            for(int j=i;j<n;++j)
             {
-                if(isConnected[i][j])
-                {
-                    int lead1 = find(i,leads);
-                    int lead2 = find(j,leads);
-                    
-                    if(lead1 != lead2)
-                    {
-                        leads[lead1] = lead2;
-                        groups--;
-                    }
-                }
+                if(isConnected[i][j]==1)edges.push_back({i,j});
             }
         }
-        
-        return groups;
-        
-    }
-    
-    private:
-    int find(int x, vector<int>& parents) {
-        return parents[x] == x ? x : find(parents[x], parents);
+        vector<int>size(n,0);
+        for(auto& it:edges)Union(it[0],it[1],par,size);
+        int ans=0;
+        for(int i=0;i<n;++i)
+        {
+            //if node is its own parent -> it's a province
+            if(i==par[i])ans++;
+        }
+        return ans;
     }
 };
