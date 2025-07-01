a7457abb395d6401264bd0aebdb8d0397d65bd81
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-07-02 00:55:23 +0530

    Time: 0 ms (100%), Space: 12.8 MB (81.74%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,63 +1,25 @@
-class DisjointSet
-{
-    public:
-    vector<int>parent;
-    vector<int>rank;
-    DisjointSet(int n)
-    {
-        parent.resize(n+1);
-        rank.resize(n+1);
-        parent[0]=0,rank[0]=0;
-        for(int i=1;i<=n;++i)
-        { 
-            parent[i]=i;
-            rank[i]=0;
-        }
-    }
-    int Find(int n)
-    {
-        if(parent[n]==n)
-        {
-            return n;
-        }
-        return parent[n]=Find(parent[n]);
-    }
-    void Union(int u,int v)
-    {
-        int a=Find(u),b=Find(v);
-        if(rank[a]<rank[b])
-        {
-            parent[a]=b;
-        }
-        else if(rank[b]<rank[a])
-        {
-            parent[b]=a;
-        }
-        else
-        {
-            parent[a]=b;
-            rank[b]++;
-        }
-    }
-};
 class Solution {
 public:
+    vector<int>par;
+    int find(int v)
+    {
+        if(v==par[v])return v;
+        return par[v]=find(par[v]);
+    }
+    int Union(int a,int b)
+    {
+        a=find(a),b=find(b);
+        if(a==b)return 1;
+        par[b]=a;
+        return 0;
+    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
-        DisjointSet Ds(n);
-        for(int i=0;i<n;++i)
+        for(int i=0;i<=n;++i)par.push_back(i);
+        for(auto& it:edges)
         {
-            int u=Ds.Find(edges[i][0]);
-            int v=Ds.Find(edges[i][1]);
-            if(u!=v)
-            {
-                Ds.Union(u,v);
-            }
-            else
-            {
-                return{edges[i][0],edges[i][1]};
-            }
+            if(Union(it[0],it[1]))return {it[0],it[1]};
         }
-        return {};
+        return {-1,-1};
     }
 };
