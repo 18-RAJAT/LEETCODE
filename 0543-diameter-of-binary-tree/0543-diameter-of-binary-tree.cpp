45e261323a37a2306e232a4783ecf8813257f0a9
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-07-04 01:12:53 +0530

    Time: 0 ms (100%), Space: 23.5 MB (99.61%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,22 +11,27 @@
  */
 class Solution {
 public:
-    int maxi=0;
-    //cache check
-    int func(TreeNode* root)
+    int recur(TreeNode* root,int& res)
     {
-        if(root==nullptr)
-        {
-            return 0;
-        }
-        int L=func(root->left);
-        int R=func(root->right);
-        maxi=max<int>(L+R,maxi);
-        
-        return 1+max<int>(L,R);
+        if(root==nullptr)return 0;
+        int l=recur(root->left,res),r=recur(root->right,res);
+        //height of the current node.
+        int height=1+max(l,r);
+        //path passing current node.
+        int ans=max(height,1+l+r);
+        res=max(res,ans);
+        //parent node.
+        return height;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        func(root);
-        return maxi;
+        int ans=0;
+        recur(root,ans);
+        /*
+            edges=node-1...
+            Longest path = 2 -> 1 -> 3
+
+            Nodes=3 ->> [2, 1, 3],Edges=2, ans=Nodes-1...
+        */
+        return ans-1;
     }
 };
