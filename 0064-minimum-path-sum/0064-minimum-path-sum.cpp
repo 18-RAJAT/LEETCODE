b546a2c4c1d38a5a742a0bc74632f38e1aa46634
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-07-06 01:42:57 +0530

    Time: 0 ms (100%), Space: 15.1 MB (98.79%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -5,16 +5,12 @@ public:
         {
             return a<b?a:b;
         };
-        int m=grid.size();
-        int n=grid[0].size();
-        for(int i=0;i<m;i++)
+        int m=grid.size(),n=grid[0].size();
+        for(int i=0;i<m;++i)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<n;++j)
             {
-                if(i==0 and j==0)
-                {
-                    continue;
-                }
+                if(i==0 && j==0)continue;
                 else if(i==0)
                 {
                     grid[i][j]+=grid[i][j-1];
