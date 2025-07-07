b5c53ad2dc62cce5c055eddbddc197fe6feb0acf
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-07-08 01:18:43 +0530

    Time: 184 ms (38.24%), Space: 257.7 MB (5.88%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,33 +1,62 @@
+struct TrieNode
+{
+    TrieNode* child[26]={};
+    bool isEnd=false;
+};
+class Trie
+{
+    TrieNode* root;
+    public:
+    Trie(){root=new TrieNode();}
+    void insert(string& s)
+    {
+        TrieNode* cur=root;
+        for(char& it:s)
+        {
+            int i=it-'a';
+            if(!cur->child[i])cur->child[i]=new TrieNode();
+            cur=cur->child[i];
+        }
+        cur->isEnd=true;
+    }
+    //DFS to check if a word can be formed by concatenating at least 2 other words
+    bool dfs(string& s,int idx,int cnt,unordered_map<int,bool>& dp)
+    {
+        if(idx==s.size())return cnt>=2;
+        if(dp.count(idx))return dp[idx];
+        TrieNode* cur=root;
+        for(int i=idx;i<s.size();++i)
+        {
+            int c=s[i]-'a';
+            if(!cur->child[c])break;
+            cur=cur->child[c];
+            if(cur->isEnd)
+            {
+                //if prefix is a word, try remaining part recursively
+                if(dfs(s,i+1,cnt+1,dp))return dp[idx]=true;
+            }
+        }
+        return dp[idx]=false;
+    }
+    bool isConcat(string& s)
+    {
+        unordered_map<int,bool>dp;
+        return dfs(s,0,0,dp);
+    }
+};
 class Solution {
 public:
-    bool dfs(string word,unordered_set<string>&s)
-    {
-        int n=word.size();
-        if(s.count(word))
+    vector<string>findAllConcatenatedWordsInADict(vector<string>& words) {
+        Trie t;
+        for(string& it:words)
         {
-            return true;
+            if(it.size())t.insert(it);
         }
-        for(int i=0;i<n;++i)
+        vector<string>ans;
+        for(string& it:words)
         {
-            if(s.count(word.substr(0,i)) and dfs(word.substr(i),s))
-            {
-                return true;
-            }
+            if(t.isConcat(it))ans.push_back(it);
         }
-        return false;
-    }
-    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        unordered_set<string>s(words.begin(),words.end());
-        vector<string>res;
-        for(auto word:words)
-        {
-            s.erase(word);
-            if(dfs(word,s))
-            {
-                res.push_back(word);
-            }
-            s.insert(word);
-        }
-        return res;      
+        return ans;
     }
 };
