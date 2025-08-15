3b4478cd4a7ae793c02e428a7838da6b22ff072d
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-08-15 10:39:11 +0530

    Time: 0 ms (100%), Space: 7.9 MB (18.14%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -3,13 +3,7 @@ public:
     bool isPowerOfFour(int n) {
         if(n==0)return false;
         if(n==1)return true;
-        if(n%4==0)
-        {
-            return isPowerOfFour(n/4);
-        }
-        else
-        {
-            return false;
-        }
+        if(n%4==0)return isPowerOfFour(n/4);
+        else return false;
     }
 };
