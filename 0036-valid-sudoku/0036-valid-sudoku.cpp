809208810e772b7a6be48c501e7adb141dbf5956
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-08-30 10:55:13 +0530

    Time: 5 ms (43.86%), Space: 23.8 MB (56.79%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        function<bool(int,int)>check=[&](int i,int j)->bool
+        vector<set<int>>rows(9),cols(9),blocks(9);
+        for(int i=0;i<9;++i)
         {
-            for(int k=0;k<9;k++)
+            for (int j=0;j<9;++j) 
             {
-                if(k!=j and board[i][k]==board[i][j])
+                
+                if (board[i][j]=='.')
+                {
+                    continue;
+                }
+                int curr=board[i][j];
+                if (rows[i].count(curr) or cols[j].count(curr) or blocks[(i/3)*3+j/3].count(curr)) 
                 {
                     return false;
                 }
-                if(k!=i and board[k][j]==board[i][j])
-                {
-                    return false;
-                }
-            }
-            int x=i/3;
-            int y=j/3;
-            for(int k=0;k<3;k++)
-            {
-                for(int l=0;l<3;l++)
-                {
-                    if((x*3+k!=i or y*3+l!=j) and board[x*3+k][y*3+l]==board[i][j])
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        };
-        for(int i=0;i<9;i++)
-        {
-            for(int j=0;j<9;j++)
-            {
-                if(board[i][j]!='.' and not check(i,j))
-                {
-                    return false;
-                }
+                
+                rows[i].insert(curr);                
+                cols[j].insert(curr);
+                
+                blocks[(i/3)*3+j/3].insert(curr);
             }
         }
         return true;
