0c94c2376be1afe0ed9cebc53969d7f250533253
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-10-06 12:33:30 +0530

    Time: 11 ms (40.2%), Space: 16.1 MB (17.56%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -0,0 +1,26 @@
+class Solution{
+public:
+int swimInWater(vector<vector<int>>& grid) {
+        int m=grid.size(),n=grid[0].size();
+        vector<tuple<int,int,int>>ar;
+        for(int i=0;i<m;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                if(i>0)ar.push_back({max(grid[i][j],grid[i-1][j]),i*n+j,(i-1)*n+j});
+                if(j>0)ar.push_back({max(grid[i][j],grid[i][j-1]),i*n+j,i*n+j-1});
+            }
+        }
+        sort(ar.begin(),ar.end());
+        vector<int>par(m*n);
+        iota(par.begin(),par.end(),0);
+        function<int(int)>find=[&](int x){return par[x]==x?x:par[x]=find(par[x]);};
+        auto unite=[&](int x,int y){par[find(x)]=find(y);};
+        for(auto& [x,y,z]:ar)
+        {
+            unite(y,z);
+            if(find(0)==find(m*n-1))return x;
+        }
+        return grid[0][0];
+    }
+};
