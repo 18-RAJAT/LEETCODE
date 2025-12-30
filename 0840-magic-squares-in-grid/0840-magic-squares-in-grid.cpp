ca7bbbd17589903b61721368f97e880646d04402
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2025-12-30 10:58:36 +0530

    Time: 3 ms (18.24%), Space: 11.4 MB (72.04%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
@@ -16,27 +16,27 @@ public:
         return count;
     }
     
-    bool judge(vector<vector<int>> &grid,int i,int j)
-    { 
-        if(grid[i][j]==5)return false;
+    bool judge(vector<vector<int>>& grid,int i,int j)
+    {
+        bool ok[10]={0};
         for(int x=i;x<i+3;++x)
-        {                                      
+        {
             for(int y=j;y<j+3;++y)
             {
-                int curele=grid[x][y];
-                if(curele>9)return false;
+                int v=grid[x][y];
+                if(v<1 || v>9 || ok[v])return false;
+                ok[v]=1;
             }
         }
         for(int x=i;x<i+3;++x)
-        {
             if(grid[x][j]+grid[x][j+1]+grid[x][j+2]!=15)return false;
-        }
+
         for(int y=j;y<j+3;++y)
-        {
             if(grid[i][y]+grid[i+1][y]+grid[i+2][y]!=15)return false;
-        }
+
         if(grid[i][j]+grid[i+1][j+1]+grid[i+2][j+2]!=15)return false;
         if(grid[i+2][j]+grid[i+1][j+1]+grid[i][j+2]!=15)return false;
+
         return true;
     }
 };
