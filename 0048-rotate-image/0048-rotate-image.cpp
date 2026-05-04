7ac2941b4531461e8880520d286e0f2f58aae854
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2026-05-04 21:22:44 +0530

    Time: 0 ms (100%), Space: 10 MB (92.7%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,43 +1,13 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        auto transpose=[&](vector<vector<int>>&mat)->void
+        reverse(matrix.begin(),matrix.end());
+        for(int i=0;i<matrix.size();++i)
         {
-            int n=mat.size();
-            // int m=mat[0].size();
-            for(int i=0;i<n;++i)
+            for(int j=0;j<=i;++j)
             {
-                for(int j=i;j<n;++j)
-                {
-                    swap(mat[i][j],mat[j][i]);
-                }
+                swap(matrix[i][j],matrix[j][i]);
             }
-        };
-        
-        /*
-           sequence
-           
-           1
-           4
-           7
-           
-           transpose
-           
-           start=7
-           start.next=4
-           start.next.next=1
-           last to first[7,4,1]
-           
-        */
-        function<void(vector<vector<int>>&)>revRow=[&](vector<vector<int>>&mat)->void
-        {
-            int n=mat.size();
-            for(int i=0;i<n;++i)
-            {
-                reverse(mat[i].begin(),mat[i].end());
-            }
-        };
-        transpose(matrix);
-        revRow(matrix);
+        }
     }
 };
