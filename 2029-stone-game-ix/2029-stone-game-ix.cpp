c95d5e800e917506f28294ac8317e94e12285ae0
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2026-08-16 09:22:49 +0530

    Time: 7 ms (39.59%), Space: 131.2 MB (69.29%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    bool stoneGameIX(vector<int>& s) {
-          vector<int>a(3,0);
-          for(auto v:s)
-          ++a[v%3];
-          if(a[0]%2){return abs(a[1]-a[2])>2;}
-          
-            return a[1]!=0 and a[2]!=0;
+    bool stoneGameIX(vector<int>& stones) {
+        vector<int>ar(3,0);
+        for(auto& it:stones)ar[it%3]++;
+        int mn=min(ar[1],ar[2]),mx=max(ar[1],ar[2]);
+        if(mn==0 && mx>2)return ar[0]%2==1;
+        else if (mn==0)return false;
+        return ar[0]%2==0 || mx>mn+2;
     }
 };
-
